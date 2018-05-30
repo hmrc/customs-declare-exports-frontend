c99e38cb96801750a43a248a1ac72328a2b32419
Benjamin Parker <benjamin@needstyping.com>
2018-05-30 15:41:30 +0100

    Is This Part Of A Consolidation - initial web spec and controller

---
+++ app/uk/gov/hmrc/decex/controllers/IsThisPartOfAConsolidation.scala
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.decex.controllers
+
+import javax.inject.{Inject, Singleton}
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc._
+import uk.gov.hmrc.decex.config.AppConfig
+import uk.gov.hmrc.play.bootstrap.controller.FrontendController
+
+import scala.concurrent.Future
+
+@Singleton
+class IsThisPartOfAConsolidation @Inject()(val messagesApi: MessagesApi, implicit val appConfig: AppConfig) extends FrontendController with I18nSupport {
+
+
+  val choose = Action.async { implicit request =>
+    Future.successful(Ok(uk.gov.hmrc.decex.views.html.choose_consolidation()))
+  }
+
+}
