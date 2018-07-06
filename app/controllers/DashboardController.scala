eaa7f8cffb45d42b3f71484ac74661f991297e62
Gabor Dorcsinecz <gabor.dorcsinecz@digital.hmrc.gov.uk>
2018-07-06 15:27:29 +0100

    Changed FrontPage

---
+++ app/controllers/DashboardController.scala
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import javax.inject.Inject
+
+import play.api.i18n.{I18nSupport, MessagesApi}
+import uk.gov.hmrc.play.bootstrap.controller.FrontendController
+import controllers.actions._
+import config.FrontendAppConfig
+import views.html.dashboard
+
+import scala.concurrent.Future
+
+class DashboardController @Inject()(appConfig: FrontendAppConfig,
+                                         override val messagesApi: MessagesApi,
+                                         authenticate: AuthAction,
+                                         getData: DataRetrievalAction,
+                                         requireData: DataRequiredAction) extends FrontendController with I18nSupport {
+
+  def onPageLoad = (authenticate) {
+    implicit request =>
+      Ok(dashboard(appConfig))
+  }
+}
---
+++ test/controllers/DashboardControllerSpec.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import controllers.actions._
+import play.api.test.Helpers._
+import views.html.dashboard
+
+class DashboardControllerSpec extends ControllerSpecBase {
+
+  def controller(dataRetrievalAction: DataRetrievalAction = getEmptyCacheMap) =
+    new DashboardController(frontendAppConfig, messagesApi, FakeAuthAction,
+      dataRetrievalAction, new DataRequiredActionImpl)
+
+  def viewAsString() = dashboard(frontendAppConfig)(fakeRequest, messages).toString
+
+  "Dashboard Controller" must {
+
+    "return OK and the correct view for a GET" in {
+      val result = controller().onPageLoad(fakeRequest)
+
+      status(result) mustBe OK
+      contentAsString(result) mustBe viewAsString()
+    }
+  }
+}
+
+
+
+
