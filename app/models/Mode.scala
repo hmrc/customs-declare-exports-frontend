af144c9df011ca171113c6bbfbc661fdb605612c
PatrykRudnicki <patrudnicki@gmail.com>
2018-07-27 15:13:02 +0100

    Commit to force new build

--- app/models/Mode.scala
+++ app/models/Mode.scala
@@ -24,7 +24,6 @@ case object CheckMode extends Mode
 case object NormalMode extends Mode
 
 object Mode {
-
   implicit val jsLiteral: JavascriptLiteral[Mode] = new JavascriptLiteral[Mode] {
     override def to(value: Mode): String = value match {
       case NormalMode => "NormalMode"
