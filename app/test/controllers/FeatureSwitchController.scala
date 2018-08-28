42848d8c18f2b5b012c3ca3e8697f1e5eb07f8b9
PatrykRudnicki <patrudnicki@gmail.com>
2018-08-28 13:06:33 +0100

    Added FeatureSwitchController and test routes

---
+++ app/test/controllers/FeatureSwitchController.scala
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package test.controllers
+
+import config.AppConfig
+import features.Feature.Feature
+import features.FeatureStatus.FeatureStatus
+import javax.inject.{Inject, Singleton}
+import play.api.mvc.{Action, AnyContent}
+import uk.gov.hmrc.play.bootstrap.controller.BaseController
+
+import scala.concurrent.Future
+
+@Singleton
+class FeatureSwitchController @Inject()(implicit val appConfig: AppConfig) extends BaseController {
+
+  def set(feature: Feature, status: FeatureStatus): Action[AnyContent] = Action.async { implicit req =>
+    appConfig.setFeatureStatus(feature, status)
+    Future.successful(Ok(s"${feature} ${status}"))
+  }
+}
