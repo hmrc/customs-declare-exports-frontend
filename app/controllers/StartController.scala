dc264c0e4e9bce5df291f0e154f935c9ba524771
NC-1234 <nick.choudhry@digital.hmrc.gov.uk>
2018-11-22 14:53:00 +0000

    CEDS-743: view start page

---
+++ app/controllers/StartController.scala
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import config.AppConfig
+import javax.inject.Inject
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, Call}
+import uk.gov.hmrc.play.bootstrap.controller.FrontendController
+import views.html.{choice_page, startPage}
+
+import scala.concurrent.Future
+
+class StartController @Inject() (
+  appConfig: AppConfig,
+  override val messagesApi: MessagesApi) extends FrontendController with I18nSupport {
+
+  def displayStartPage(): Action[AnyContent] = Action.async { implicit request =>
+    Future.successful(Ok(startPage(appConfig)))
+  }
+
+}
