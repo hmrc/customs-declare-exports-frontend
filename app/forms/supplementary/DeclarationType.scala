24403d22b61489bc1bcf90ee017ea108a1a64c6d
Maciej Rewera <ReweraM@gmail.com>
2018-12-18 17:01:50 +0000

    [CEDS-807] Extracted Declaration Type values to separate objects

--- app/forms/supplementary/DeclarationType.scala
+++ app/forms/supplementary/DeclarationType.scala
@@ -35,8 +35,14 @@ case class DeclarationType(
 object DeclarationType {
   implicit val format = Json.format[DeclarationType]
 
-  private val declarationTypeAllowedValues = Set("CO", "EX")
-  private val additionalDeclarationTypeAllowedValues = Set("Y", "Z")
+  private val declarationTypeAllowedValues = Set(
+    AllowedTypes.OutsideEU,
+    AllowedTypes.FiscalTerritory
+  )
+  private val additionalDeclarationTypeAllowedValues = Set(
+    AllowedAdditionalTypes.Simplified,
+    AllowedAdditionalTypes.Standard
+  )
 
   val formId = "DeclarationTypeId"
 
@@ -50,4 +56,14 @@ object DeclarationType {
   )(DeclarationType.apply)(DeclarationType.unapply)
 
   def form(): Form[DeclarationType] = Form(mapping)
+
+  object AllowedTypes {
+    val OutsideEU = "EX"
+    val FiscalTerritory = "CO"
+  }
+
+  object AllowedAdditionalTypes {
+    val Simplified = "Y"
+    val Standard = "Z"
+  }
 }
