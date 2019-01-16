83efdd6ce07fd21e2a31288a112d33f72b36a7c4
Jodlowy <Patryk.hb@gmail.com>
2019-01-16 16:38:34 +0000

    CEDS-881 Total Amount Invoiced

--- app/forms/supplementary/TotalNumberOfItems.scala
+++ app/forms/supplementary/TotalNumberOfItems.scala
@@ -17,24 +17,35 @@
 package forms.supplementary
 
 import play.api.data.{Form, Forms}
-import play.api.data.Forms.text
+import play.api.data.Forms.{optional, text}
 import play.api.libs.json.Json
 import utils.validators.FormFieldValidator._
 
-case class TotalNumberOfItems(itemsNo: String)
+case class TotalNumberOfItems(itemsNo: String, totalAmountInvoiced: Option[String], exchangeRate: Option[String])
 
 object TotalNumberOfItems {
   implicit val format = Json.format[TotalNumberOfItems]
 
   val formId = "TotalNumberOfItems"
 
+  val totalAmountInvoicedPattern = "[0-9]{1,14}|[0-9]{1,14}[.][0-9]{1,2}"
+  val exchangeRatePattern = "[0-9]{1,12}|[[0-9]{1,7}[.][0-9]{1,5}]{3,13}"
+
   val mapping = Forms.mapping(
     "items" -> text()
-      .verifying("supplementary.totalNumberOfItems.error", isNumeric and noLongerThan(3) and forbiddenZero)
+      .verifying("supplementary.totalNumberOfItems.error", isNumeric and noLongerThan(3) and forbiddenZero),
+    "totalAmountInvoiced" -> optional(
+      text().verifying("supplementary.totalAmountInvoiced.error", x => x.matches(totalAmountInvoicedPattern))
+    ),
+    "exchangeRate" -> optional(text().verifying("supplementary.exchangeRate.error", _.matches(exchangeRatePattern)))
   )(TotalNumberOfItems.apply)(TotalNumberOfItems.unapply)
 
   def form(): Form[TotalNumberOfItems] = Form(mapping)
 
   def toMetadataProperties(numberOfItems: TotalNumberOfItems): Map[String, String] =
-    Map("declaration.goodsItemQuantity" -> numberOfItems.itemsNo)
+    Map(
+      "declaration.goodsItemQuantity" -> numberOfItems.itemsNo,
+      "declaration.invoiceAmount" -> numberOfItems.totalAmountInvoiced.getOrElse(""),
+      "declaration.currencyExchange.rateNumeric" -> numberOfItems.exchangeRate.getOrElse("")
+    )
 }
