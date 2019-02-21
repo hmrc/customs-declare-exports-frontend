024fc1ad1269d42b865ae330afd295314f08fa90
Maciej Rewera <22579925+MaciejRewera@users.noreply.github.com>
2019-02-21 16:12:03 +0000

    CEDS-969 Add quantity field to documents produced and summary pages

--- test/controllers/supplementary/DocumentsProducedControllerSpec.scala
+++ test/controllers/supplementary/DocumentsProducedControllerSpec.scala
@@ -19,26 +19,26 @@ package controllers.supplementary
 import base.{CustomExportsBaseSpec, TestHelper}
 import forms.supplementary.DocumentsProduced
 import forms.supplementary.DocumentsProducedSpec._
+import org.scalatest.BeforeAndAfter
 import play.api.libs.json.{JsObject, JsString, JsValue}
 import play.api.test.Helpers._
 
-class DocumentsProducedControllerSpec extends CustomExportsBaseSpec {
+class DocumentsProducedControllerSpec extends CustomExportsBaseSpec with BeforeAndAfter {
+
+  before {
+    authorizedUser()
+    withCaching[DocumentsProduced](None)
+  }
 
   val uri = uriWithContextPath("/declaration/supplementary/add-document")
 
   "DocumentsProducedController" should {
     "return 200 with a success" in {
-      authorizedUser()
-      withCaching[DocumentsProduced](None)
-
       val result = route(app, getRequest(uri)).get
       status(result) must be(OK)
     }
 
     "display add document form" in {
-      authorizedUser()
-      withCaching[DocumentsProduced](None)
-
       val result = route(app, getRequest(uri)).get
       val stringResult = contentAsString(result)
 
@@ -50,78 +50,72 @@ class DocumentsProducedControllerSpec extends CustomExportsBaseSpec {
       stringResult must include(messages("supplementary.addDocument.documentPart"))
       stringResult must include(messages("supplementary.addDocument.documentStatus"))
       stringResult must include(messages("supplementary.addDocument.documentStatusReason"))
+      stringResult must include(messages("supplementary.addDocument.documentQuantity"))
     }
 
-    "validate form - incorrect document status" in {
-      authorizedUser()
-      withCaching[DocumentsProduced](None)
+    "display page with errors" when {
+      "provided with incorrect document status" in {
+        val incorrectDocumentStatus: JsValue = JsObject(Map("documentStatus" -> JsString("as")))
 
-      val incorrectDocumentStatus: JsValue = JsObject(Map("documentStatus" -> JsString("as")))
+        val result = route(app, postRequest(uri, incorrectDocumentStatus)).get
+        status(result) must be(BAD_REQUEST)
+        contentAsString(result) must include(messages("supplementary.addDocument.documentStatus.error"))
+      }
 
-      val result = route(app, postRequest(uri, incorrectDocumentStatus)).get
-      status(result) must be(BAD_REQUEST)
-      contentAsString(result) must include(messages("supplementary.addDocument.documentStatus.error"))
+      "provided with incorrect document status reason" in {
+        val incorrectDocumentStatusReason: JsValue =
+          JsObject(Map("documentStatusReason" -> JsString(TestHelper.createRandomString(36))))
 
+        val result = route(app, postRequest(uri, incorrectDocumentStatusReason)).get
+        status(result) must be(BAD_REQUEST)
+        contentAsString(result) must include(messages("supplementary.addDocument.documentStatusReason.error"))
+      }
+
+      "provided with incorrect document identifier" in {
+        val incorrectDocumentStatusReason: JsValue =
+          JsObject(Map("documentIdentifier" -> JsString(TestHelper.createRandomString(31))))
+
+        val result = route(app, postRequest(uri, incorrectDocumentStatusReason)).get
+        status(result) must be(BAD_REQUEST)
+        contentAsString(result) must include(messages("supplementary.addDocument.documentIdentifier.error"))
+      }
+
+      "provided with incorrect document part" in {
+        val incorrectDocumentStatusReason: JsValue =
+          JsObject(Map("documentPart" -> JsString(TestHelper.createRandomString(6))))
+
+        val result = route(app, postRequest(uri, incorrectDocumentStatusReason)).get
+        status(result) must be(BAD_REQUEST)
+        contentAsString(result) must include(messages("supplementary.addDocument.documentPart.error"))
+      }
+      
+      "provided with incorrect documents quantity" in {
+        val incorrectDocumentQuantity: JsValue =
+          JsObject(Map("documentQuantity" -> JsString("123456789012.1234567")))
+
+        val result = route(app, postRequest(uri, incorrectDocumentQuantity)).get
+        status(result) must be(BAD_REQUEST)
+        contentAsString(result) must include(messages("supplementary.addDocument.documentQuantity.error"))
+      }
+    }
+    
+    "redirect to summary page" when {
+      "provided with empty form" in {
+        val result = route(app, postRequest(uri, emptyDocumentsProducedJSON)).get
+        val header = result.futureValue.header
+
+        status(result) must be(SEE_OTHER)
+        header.headers.get("Location") must be(Some("/customs-declare-exports/declaration/supplementary/summary"))
+      }
+
+      "provided with correct form" in {
+        val result = route(app, postRequest(uri, correctDocumentsProducedJSON)).get
+        val header = result.futureValue.header
+
+        status(result) must be(SEE_OTHER)
+        header.headers.get("Location") must be(Some("/customs-declare-exports/declaration/supplementary/summary"))
+      }
     }
 
-    "validate form - incorrect Document status reason" in {
-      authorizedUser()
-      withCaching[DocumentsProduced](None)
-
-      val incorrectDocumentStatusReason: JsValue =
-        JsObject(Map("documentStatusReason" -> JsString(TestHelper.createRandomString(36))))
-
-      val result = route(app, postRequest(uri, incorrectDocumentStatusReason)).get
-      status(result) must be(BAD_REQUEST)
-      contentAsString(result) must include(messages("supplementary.addDocument.documentStatusReason.error"))
-    }
-
-    "validate form - incorrect document identifier" in {
-      authorizedUser()
-      withCaching[DocumentsProduced](None)
-
-      val incorrectDocumentStatusReason: JsValue =
-        JsObject(Map("documentIdentifier" -> JsString(TestHelper.createRandomString(31))))
-
-      val result = route(app, postRequest(uri, incorrectDocumentStatusReason)).get
-      status(result) must be(BAD_REQUEST)
-      contentAsString(result) must include(messages("supplementary.addDocument.documentIdentifier.error"))
-    }
-
-    "validate form - incorrect document part" in {
-      authorizedUser()
-      withCaching[DocumentsProduced](None)
-
-      val incorrectDocumentStatusReason: JsValue =
-        JsObject(Map("documentPart" -> JsString(TestHelper.createRandomString(6))))
-
-      val result = route(app, postRequest(uri, incorrectDocumentStatusReason)).get
-      status(result) must be(BAD_REQUEST)
-      contentAsString(result) must include(messages("supplementary.addDocument.documentPart.error"))
-    }
-
-    "validate form - empty form" in {
-      pending
-      authorizedUser()
-      withCaching[DocumentsProduced](None)
-
-      val result = route(app, postRequest(uri, emptyDocumentsProducedJSON)).get
-      val header = result.futureValue.header
-
-      status(result) must be(SEE_OTHER)
-      header.headers.get("Location") must be(Some("/customs-declare-exports/declaration/supplementary/summary"))
-    }
-
-    "validate form - correct form" in {
-      pending
-      authorizedUser()
-      withCaching[DocumentsProduced](None)
-
-      val result = route(app, postRequest(uri, correctDocumentsProducedJSON)).get
-      val header = result.futureValue.header
-
-      status(result) must be(SEE_OTHER)
-      header.headers.get("Location") must be(Some("/customs-declare-exports/declaration/supplementary/summary"))
-    }
   }
 }
