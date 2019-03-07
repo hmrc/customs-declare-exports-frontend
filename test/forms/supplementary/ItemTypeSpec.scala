6e52ac1ba44a21e5cda641d15e3ff2cf351deb04
Maciej Rewera <22579925+MaciejRewera@users.noreply.github.com>
2019-03-07 10:34:57 +0000

    CEDS-944, CEDS-945 Added tests using MetaData for ItemType
    This is an enhancement into how to test mapping into MetaData. It is
    not prone to misspelling.

--- test/forms/supplementary/ItemTypeSpec.scala
+++ test/forms/supplementary/ItemTypeSpec.scala
@@ -19,6 +19,7 @@ package forms.supplementary
 import forms.supplementary.ItemType._
 import org.scalatest.{MustMatchers, WordSpec}
 import play.api.libs.json.{JsArray, JsObject, JsString, JsValue}
+import uk.gov.hmrc.wco.dec.MetaData
 
 class ItemTypeSpec extends WordSpec with MustMatchers {
   import ItemTypeSpec._
@@ -206,6 +207,282 @@ class ItemTypeSpec extends WordSpec with MustMatchers {
         )
 
         itemType.toMetadataProperties() must equal(expectedItemTypeProperties)
+      }
+    }
+  }
+
+  "ItemType converted into MetaData" should {
+
+    "contain proper values" when {
+      "provided with mandatory data only" in {
+        val itemType = ItemType(
+          combinedNomenclatureCode = combinedNomenclatureCode,
+          taricAdditionalCodes = Nil,
+          nationalAdditionalCodes = Nil,
+          descriptionOfGoods = descriptionOfGoods,
+          cusCode = None,
+          statisticalValue = statisticalValue
+        )
+
+        val metadata = MetaData.fromProperties(itemType.toMetadataProperties())
+
+        metadata.declaration must be(defined)
+        metadata.declaration.get.goodsShipment must be(defined)
+        metadata.declaration.get.goodsShipment.get.governmentAgencyGoodsItems mustNot be(empty)
+
+        val governmentAgencyGoodsItem = metadata.declaration.get.goodsShipment.get.governmentAgencyGoodsItems.head
+        governmentAgencyGoodsItem.commodity must be(defined)
+        governmentAgencyGoodsItem.commodity.get.classifications.size must equal(1)
+        governmentAgencyGoodsItem.commodity.get.classifications.head.id must be(defined)
+        governmentAgencyGoodsItem.commodity.get.classifications.head.id.get must equal(combinedNomenclatureCode)
+        governmentAgencyGoodsItem.commodity.get.classifications.head.identificationTypeCode must be(defined)
+        governmentAgencyGoodsItem.commodity.get.classifications.head.identificationTypeCode.get must equal(IdentificationTypeCodes.CombinedNomenclatureCode)
+
+        governmentAgencyGoodsItem.commodity.get.description must be(defined)
+        governmentAgencyGoodsItem.commodity.get.description.get must equal(descriptionOfGoods)
+        governmentAgencyGoodsItem.statisticalValueAmount must be(defined)
+        governmentAgencyGoodsItem.statisticalValueAmount.get.value must be(defined)
+        governmentAgencyGoodsItem.statisticalValueAmount.get.value.get.toString() must equal(statisticalValue)
+      }
+
+      "provided with mandatory data and single TARIC Additional Code" in {
+        val itemType = ItemType(
+          combinedNomenclatureCode = combinedNomenclatureCode,
+          taricAdditionalCodes = Seq(taricAdditionalCode),
+          nationalAdditionalCodes = Nil,
+          descriptionOfGoods = descriptionOfGoods,
+          cusCode = None,
+          statisticalValue = statisticalValue
+        )
+
+        val metadata = MetaData.fromProperties(itemType.toMetadataProperties())
+
+        metadata.declaration must be(defined)
+        metadata.declaration.get.goodsShipment must be(defined)
+        metadata.declaration.get.goodsShipment.get.governmentAgencyGoodsItems mustNot be(empty)
+
+        val governmentAgencyGoodsItem = metadata.declaration.get.goodsShipment.get.governmentAgencyGoodsItems.head
+        governmentAgencyGoodsItem.commodity must be(defined)
+        governmentAgencyGoodsItem.commodity.get.classifications.size must equal(2)
+        governmentAgencyGoodsItem.commodity.get.classifications.head.id must be(defined)
+        governmentAgencyGoodsItem.commodity.get.classifications.head.id.get must equal(combinedNomenclatureCode)
+        governmentAgencyGoodsItem.commodity.get.classifications.head.identificationTypeCode must be(defined)
+        governmentAgencyGoodsItem.commodity.get.classifications.head.identificationTypeCode.get must equal(IdentificationTypeCodes.CombinedNomenclatureCode)
+        governmentAgencyGoodsItem.commodity.get.classifications(1).id must be(defined)
+        governmentAgencyGoodsItem.commodity.get.classifications(1).id.get must equal(taricAdditionalCode)
+        governmentAgencyGoodsItem.commodity.get.classifications(1).identificationTypeCode must be(defined)
+        governmentAgencyGoodsItem.commodity.get.classifications(1).identificationTypeCode.get must equal(IdentificationTypeCodes.TARICAdditionalCode)
+
+        governmentAgencyGoodsItem.commodity.get.description must be(defined)
+        governmentAgencyGoodsItem.commodity.get.description.get must equal(descriptionOfGoods)
+        governmentAgencyGoodsItem.statisticalValueAmount must be(defined)
+        governmentAgencyGoodsItem.statisticalValueAmount.get.value must be(defined)
+        governmentAgencyGoodsItem.statisticalValueAmount.get.value.get.toString() must equal(statisticalValue)
+      }
+
+      "provided with mandatory data and multiple TARIC Additional Codes" in {
+        val taricAdditionalCode_1 = "AB12"
+        val taricAdditionalCode_2 = "CD34"
+        val taricAdditionalCode_3 = "56EF"
+        val itemType = ItemType(
+          combinedNomenclatureCode = combinedNomenclatureCode,
+          taricAdditionalCodes = Seq(taricAdditionalCode_1, taricAdditionalCode_2, taricAdditionalCode_3),
+          nationalAdditionalCodes = Nil,
+          descriptionOfGoods = descriptionOfGoods,
+          cusCode = None,
+          statisticalValue = statisticalValue
+        )
+
+        val metadata = MetaData.fromProperties(itemType.toMetadataProperties())
+
+        metadata.declaration must be(defined)
+        metadata.declaration.get.goodsShipment must be(defined)
+        metadata.declaration.get.goodsShipment.get.governmentAgencyGoodsItems mustNot be(empty)
+
+        val governmentAgencyGoodsItem = metadata.declaration.get.goodsShipment.get.governmentAgencyGoodsItems.head
+        governmentAgencyGoodsItem.commodity must be(defined)
+        governmentAgencyGoodsItem.commodity.get.classifications.size must equal(4)
+        governmentAgencyGoodsItem.commodity.get.classifications.head.id must be(defined)
+        governmentAgencyGoodsItem.commodity.get.classifications.head.id.get must equal(combinedNomenclatureCode)
+        governmentAgencyGoodsItem.commodity.get.classifications.head.identificationTypeCode must be(defined)
+        governmentAgencyGoodsItem.commodity.get.classifications.head.identificationTypeCode.get must equal(IdentificationTypeCodes.CombinedNomenclatureCode)
+        governmentAgencyGoodsItem.commodity.get.classifications(1).id must be(defined)
+        governmentAgencyGoodsItem.commodity.get.classifications(1).id.get must equal(taricAdditionalCode_1)
+        governmentAgencyGoodsItem.commodity.get.classifications(1).identificationTypeCode must be(defined)
+        governmentAgencyGoodsItem.commodity.get.classifications(1).identificationTypeCode.get must equal(IdentificationTypeCodes.TARICAdditionalCode)
+        governmentAgencyGoodsItem.commodity.get.classifications(2).id must be(defined)
+        governmentAgencyGoodsItem.commodity.get.classifications(2).id.get must equal(taricAdditionalCode_2)
+        governmentAgencyGoodsItem.commodity.get.classifications(2).identificationTypeCode must be(defined)
+        governmentAgencyGoodsItem.commodity.get.classifications(2).identificationTypeCode.get must equal(IdentificationTypeCodes.TARICAdditionalCode)
+        governmentAgencyGoodsItem.commodity.get.classifications(3).id must be(defined)
+        governmentAgencyGoodsItem.commodity.get.classifications(3).id.get must equal(taricAdditionalCode_3)
+        governmentAgencyGoodsItem.commodity.get.classifications(3).identificationTypeCode must be(defined)
+        governmentAgencyGoodsItem.commodity.get.classifications(3).identificationTypeCode.get must equal(IdentificationTypeCodes.TARICAdditionalCode)
+
+        governmentAgencyGoodsItem.commodity.get.description must be(defined)
+        governmentAgencyGoodsItem.commodity.get.description.get must equal(descriptionOfGoods)
+        governmentAgencyGoodsItem.statisticalValueAmount must be(defined)
+        governmentAgencyGoodsItem.statisticalValueAmount.get.value must be(defined)
+        governmentAgencyGoodsItem.statisticalValueAmount.get.value.get.toString() must equal(statisticalValue)
+      }
+
+      "provided with mandatory data and single National Additional Code" in {
+        val itemType = ItemType(
+          combinedNomenclatureCode = combinedNomenclatureCode,
+          taricAdditionalCodes = Nil,
+          nationalAdditionalCodes = Seq(nationalAdditionalCode),
+          descriptionOfGoods = descriptionOfGoods,
+          cusCode = None,
+          statisticalValue = statisticalValue
+        )
+
+        val metadata = MetaData.fromProperties(itemType.toMetadataProperties())
+
+        metadata.declaration must be(defined)
+        metadata.declaration.get.goodsShipment must be(defined)
+        metadata.declaration.get.goodsShipment.get.governmentAgencyGoodsItems mustNot be(empty)
+
+        val governmentAgencyGoodsItem = metadata.declaration.get.goodsShipment.get.governmentAgencyGoodsItems.head
+        governmentAgencyGoodsItem.commodity must be(defined)
+        governmentAgencyGoodsItem.commodity.get.classifications.size must equal(2)
+        governmentAgencyGoodsItem.commodity.get.classifications.head.id must be(defined)
+        governmentAgencyGoodsItem.commodity.get.classifications.head.id.get must equal(combinedNomenclatureCode)
+        governmentAgencyGoodsItem.commodity.get.classifications.head.identificationTypeCode must be(defined)
+        governmentAgencyGoodsItem.commodity.get.classifications.head.identificationTypeCode.get must equal(IdentificationTypeCodes.CombinedNomenclatureCode)
+        governmentAgencyGoodsItem.commodity.get.classifications(1).id must be(defined)
+        governmentAgencyGoodsItem.commodity.get.classifications(1).id.get must equal(nationalAdditionalCode)
+        governmentAgencyGoodsItem.commodity.get.classifications(1).identificationTypeCode must be(defined)
+        governmentAgencyGoodsItem.commodity.get.classifications(1).identificationTypeCode.get must equal(IdentificationTypeCodes.NationalAdditionalCode)
+
+        governmentAgencyGoodsItem.commodity.get.description must be(defined)
+        governmentAgencyGoodsItem.commodity.get.description.get must equal(descriptionOfGoods)
+        governmentAgencyGoodsItem.statisticalValueAmount must be(defined)
+        governmentAgencyGoodsItem.statisticalValueAmount.get.value must be(defined)
+        governmentAgencyGoodsItem.statisticalValueAmount.get.value.get.toString() must equal(statisticalValue)
+      }
+
+      "provided with mandatory data and multiple National Additional Codes" in {
+        val nationalAdditionalCode_1 = "AB12"
+        val nationalAdditionalCode_2 = "CD34"
+        val nationalAdditionalCode_3 = "56EF"
+        val itemType = ItemType(
+          combinedNomenclatureCode = combinedNomenclatureCode,
+          taricAdditionalCodes = Nil,
+          nationalAdditionalCodes = Seq(nationalAdditionalCode_1, nationalAdditionalCode_2, nationalAdditionalCode_3),
+          descriptionOfGoods = descriptionOfGoods,
+          cusCode = None,
+          statisticalValue = statisticalValue
+        )
+        
+        val metadata = MetaData.fromProperties(itemType.toMetadataProperties())
+
+        metadata.declaration must be(defined)
+        metadata.declaration.get.goodsShipment must be(defined)
+        metadata.declaration.get.goodsShipment.get.governmentAgencyGoodsItems mustNot be(empty)
+
+        val governmentAgencyGoodsItem = metadata.declaration.get.goodsShipment.get.governmentAgencyGoodsItems.head
+        governmentAgencyGoodsItem.commodity must be(defined)
+        governmentAgencyGoodsItem.commodity.get.classifications.size must equal(4)
+        governmentAgencyGoodsItem.commodity.get.classifications.head.id must be(defined)
+        governmentAgencyGoodsItem.commodity.get.classifications.head.id.get must equal(combinedNomenclatureCode)
+        governmentAgencyGoodsItem.commodity.get.classifications.head.identificationTypeCode must be(defined)
+        governmentAgencyGoodsItem.commodity.get.classifications.head.identificationTypeCode.get must equal(IdentificationTypeCodes.CombinedNomenclatureCode)
+        governmentAgencyGoodsItem.commodity.get.classifications(1).id must be(defined)
+        governmentAgencyGoodsItem.commodity.get.classifications(1).id.get must equal(nationalAdditionalCode_1)
+        governmentAgencyGoodsItem.commodity.get.classifications(1).identificationTypeCode must be(defined)
+        governmentAgencyGoodsItem.commodity.get.classifications(1).identificationTypeCode.get must equal(IdentificationTypeCodes.NationalAdditionalCode)
+        governmentAgencyGoodsItem.commodity.get.classifications(2).id must be(defined)
+        governmentAgencyGoodsItem.commodity.get.classifications(2).id.get must equal(nationalAdditionalCode_2)
+        governmentAgencyGoodsItem.commodity.get.classifications(2).identificationTypeCode must be(defined)
+        governmentAgencyGoodsItem.commodity.get.classifications(2).identificationTypeCode.get must equal(IdentificationTypeCodes.NationalAdditionalCode)
+        governmentAgencyGoodsItem.commodity.get.classifications(3).id must be(defined)
+        governmentAgencyGoodsItem.commodity.get.classifications(3).id.get must equal(nationalAdditionalCode_3)
+        governmentAgencyGoodsItem.commodity.get.classifications(3).identificationTypeCode must be(defined)
+        governmentAgencyGoodsItem.commodity.get.classifications(3).identificationTypeCode.get must equal(IdentificationTypeCodes.NationalAdditionalCode)
+
+        governmentAgencyGoodsItem.commodity.get.description must be(defined)
+        governmentAgencyGoodsItem.commodity.get.description.get must equal(descriptionOfGoods)
+        governmentAgencyGoodsItem.statisticalValueAmount must be(defined)
+        governmentAgencyGoodsItem.statisticalValueAmount.get.value must be(defined)
+        governmentAgencyGoodsItem.statisticalValueAmount.get.value.get.toString() must equal(statisticalValue)
+      }
+
+      "provided with mandatory data and CUS Code" in {
+        val itemType = ItemType(
+          combinedNomenclatureCode = combinedNomenclatureCode,
+          taricAdditionalCodes = Nil,
+          nationalAdditionalCodes = Nil,
+          descriptionOfGoods = descriptionOfGoods,
+          cusCode = Some(cusCode),
+          statisticalValue = statisticalValue
+        )
+        
+        val metadata = MetaData.fromProperties(itemType.toMetadataProperties())
+
+        metadata.declaration must be(defined)
+        metadata.declaration.get.goodsShipment must be(defined)
+        metadata.declaration.get.goodsShipment.get.governmentAgencyGoodsItems mustNot be(empty)
+
+        val governmentAgencyGoodsItem = metadata.declaration.get.goodsShipment.get.governmentAgencyGoodsItems.head
+        governmentAgencyGoodsItem.commodity must be(defined)
+        governmentAgencyGoodsItem.commodity.get.classifications.size must equal(2)
+        governmentAgencyGoodsItem.commodity.get.classifications.head.id must be(defined)
+        governmentAgencyGoodsItem.commodity.get.classifications.head.id.get must equal(combinedNomenclatureCode)
+        governmentAgencyGoodsItem.commodity.get.classifications.head.identificationTypeCode must be(defined)
+        governmentAgencyGoodsItem.commodity.get.classifications.head.identificationTypeCode.get must equal(IdentificationTypeCodes.CombinedNomenclatureCode)
+        governmentAgencyGoodsItem.commodity.get.classifications(1).id must be(defined)
+        governmentAgencyGoodsItem.commodity.get.classifications(1).id.get must equal(cusCode)
+        governmentAgencyGoodsItem.commodity.get.classifications(1).identificationTypeCode must be(defined)
+        governmentAgencyGoodsItem.commodity.get.classifications(1).identificationTypeCode.get must equal(IdentificationTypeCodes.CUSCode)
+
+        governmentAgencyGoodsItem.commodity.get.description must be(defined)
+        governmentAgencyGoodsItem.commodity.get.description.get must equal(descriptionOfGoods)
+        governmentAgencyGoodsItem.statisticalValueAmount must be(defined)
+        governmentAgencyGoodsItem.statisticalValueAmount.get.value must be(defined)
+        governmentAgencyGoodsItem.statisticalValueAmount.get.value.get.toString() must equal(statisticalValue)
+      }
+
+      "provided with all data" in {
+        val itemType = ItemType(
+          combinedNomenclatureCode = combinedNomenclatureCode,
+          taricAdditionalCodes = Seq(taricAdditionalCode),
+          nationalAdditionalCodes = Seq(nationalAdditionalCode),
+          descriptionOfGoods = descriptionOfGoods,
+          cusCode = Some(cusCode),
+          statisticalValue = statisticalValue
+        )
+
+        val metadata = MetaData.fromProperties(itemType.toMetadataProperties())
+
+        metadata.declaration must be(defined)
+        metadata.declaration.get.goodsShipment must be(defined)
+        metadata.declaration.get.goodsShipment.get.governmentAgencyGoodsItems mustNot be(empty)
+
+        val governmentAgencyGoodsItem = metadata.declaration.get.goodsShipment.get.governmentAgencyGoodsItems.head
+        governmentAgencyGoodsItem.commodity must be(defined)
+        governmentAgencyGoodsItem.commodity.get.classifications.size must equal(4)
+        governmentAgencyGoodsItem.commodity.get.classifications.head.id must be(defined)
+        governmentAgencyGoodsItem.commodity.get.classifications.head.id.get must equal(combinedNomenclatureCode)
+        governmentAgencyGoodsItem.commodity.get.classifications.head.identificationTypeCode must be(defined)
+        governmentAgencyGoodsItem.commodity.get.classifications.head.identificationTypeCode.get must equal(IdentificationTypeCodes.CombinedNomenclatureCode)
+        governmentAgencyGoodsItem.commodity.get.classifications(1).id must be(defined)
+        governmentAgencyGoodsItem.commodity.get.classifications(1).id.get must equal(taricAdditionalCode)
+        governmentAgencyGoodsItem.commodity.get.classifications(1).identificationTypeCode must be(defined)
+        governmentAgencyGoodsItem.commodity.get.classifications(1).identificationTypeCode.get must equal(IdentificationTypeCodes.TARICAdditionalCode)
+        governmentAgencyGoodsItem.commodity.get.classifications(2).id must be(defined)
+        governmentAgencyGoodsItem.commodity.get.classifications(2).id.get must equal(nationalAdditionalCode)
+        governmentAgencyGoodsItem.commodity.get.classifications(2).identificationTypeCode must be(defined)
+        governmentAgencyGoodsItem.commodity.get.classifications(2).identificationTypeCode.get must equal(IdentificationTypeCodes.NationalAdditionalCode)
+        governmentAgencyGoodsItem.commodity.get.classifications(3).id must be(defined)
+        governmentAgencyGoodsItem.commodity.get.classifications(3).id.get must equal(cusCode)
+        governmentAgencyGoodsItem.commodity.get.classifications(3).identificationTypeCode must be(defined)
+        governmentAgencyGoodsItem.commodity.get.classifications(3).identificationTypeCode.get must equal(IdentificationTypeCodes.CUSCode)
+
+        governmentAgencyGoodsItem.commodity.get.description must be(defined)
+        governmentAgencyGoodsItem.commodity.get.description.get must equal(descriptionOfGoods)
+        governmentAgencyGoodsItem.statisticalValueAmount must be(defined)
+        governmentAgencyGoodsItem.statisticalValueAmount.get.value must be(defined)
+        governmentAgencyGoodsItem.statisticalValueAmount.get.value.get.toString() must equal(statisticalValue)
       }
     }
   }
