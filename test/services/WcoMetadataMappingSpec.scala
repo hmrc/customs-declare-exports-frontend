83df998eb687d84c2145bd61e9d7a8511f62beb4
Maciej Rewera <22579925+MaciejRewera@users.noreply.github.com>
2019-04-01 11:31:40 +0100

    Upgrade wco-dec version to 0.26.0

--- test/services/WcoMetadataMappingSpec.scala
+++ test/services/WcoMetadataMappingSpec.scala
@@ -36,36 +36,36 @@ class WcoMetadataMappingSpec extends CustomExportsBaseSpec with GoodsItemCaching
 
       result.declaration.flatMap(_.typeCode) mustBe Some("EXY")
       result.declaration.flatMap(_.functionalReferenceId) mustBe Some("123ABC")
-      result.declaration.flatMap(_.goodsShipment.flatMap(_.ucr.flatMap(_.traderAssignedReferenceId))) mustBe
+      result.declaration.flatMap(_.goodsShipment).flatMap(_.ucr).flatMap(_.traderAssignedReferenceId) mustBe
         Some("8GB123456789012-1234567890QWERTYUIO")
 
       result.declaration.flatMap(_.exporter) must be(defined)
       result.declaration.flatMap(_.declarant) must be(defined)
       result.declaration.flatMap(_.agent) must be(defined)
       result.declaration.flatMap(_.goodsShipment) must be(defined)
-      result.declaration.flatMap(_.invoiceAmount.flatMap(_.value)) must be(Some(BigDecimal("12312312312312.12")))
-      result.declaration.flatMap(_.invoiceAmount.flatMap(_.currencyId)) must be(Some("GBP"))
+      result.declaration.flatMap(_.invoiceAmount).flatMap(_.value) must be(Some(BigDecimal("12312312312312.12")))
+      result.declaration.flatMap(_.invoiceAmount).flatMap(_.currencyId) must be(Some("GBP"))
       val goodsShipment = result.declaration.flatMap(_.goodsShipment).value
       goodsShipment.aeoMutualRecognitionParties.headOption mustBe defined
       result.declaration.value.authorisationHolders.size mustBe 1
-      result.declaration.flatMap(_.supervisingOffice.flatMap(_.id)) mustBe Some("12345678")
-      result.declaration.flatMap(_.exitOffice.flatMap(_.id)) mustBe Some("123qwe12")
-      result.declaration.flatMap(_.borderTransportMeans.flatMap(_.id)) mustBe Some("QWERTY")
-      result.declaration.flatMap(_.borderTransportMeans.flatMap(_.modeCode)) mustBe Some(3)
-      result.declaration.flatMap(_.borderTransportMeans.flatMap(_.identificationTypeCode)) mustBe Some("11")
-      result.declaration.flatMap(_.borderTransportMeans.flatMap(_.registrationNationalityCode)) mustBe Some("GB")
+      result.declaration.flatMap(_.supervisingOffice).flatMap(_.id) mustBe Some("12345678")
+      result.declaration.flatMap(_.exitOffice).flatMap(_.id) mustBe Some("123qwe12")
+      result.declaration.flatMap(_.borderTransportMeans).flatMap(_.id) mustBe Some("QWERTY")
+      result.declaration.flatMap(_.borderTransportMeans).flatMap(_.modeCode) mustBe Some(3)
+      result.declaration.flatMap(_.borderTransportMeans).flatMap(_.identificationTypeCode) mustBe Some("11")
+      result.declaration.flatMap(_.borderTransportMeans).flatMap(_.registrationNationalityCode) mustBe Some("GB")
       goodsShipment.destination.flatMap(_.countryCode) mustBe Some("PL")
       goodsShipment.exportCountry.map(_.id) mustBe Some("PL")
       goodsShipment.warehouse.flatMap(_.id) mustBe Some("1234567GB")
       goodsShipment.warehouse.map(_.typeCode) mustBe Some("R")
       goodsShipment.consignment must be(defined)
       goodsShipment.consignment.flatMap(_.goodsLocation) must be(defined)
-      goodsShipment.consignment.flatMap(_.goodsLocation.flatMap(_.address)) must be(defined)
-      goodsShipment.consignment.flatMap(_.goodsLocation.map(_.id)) mustBe Some("Additional identifier")
-      goodsShipment.consignment.flatMap(_.goodsLocation.flatMap(_.typeCode)) mustBe Some("T")
-      goodsShipment.consignment.flatMap(_.goodsLocation.flatMap(_.address)) must be(defined)
-      goodsShipment.consignment.flatMap(_.departureTransportMeans.flatMap(_.id)) must be(defined)
-      goodsShipment.consignment.flatMap(_.departureTransportMeans.flatMap(_.identificationTypeCode)) must be(defined)
+      goodsShipment.consignment.flatMap(_.goodsLocation).flatMap(_.address) must be(defined)
+      goodsShipment.consignment.flatMap(_.goodsLocation).flatMap(_.id) mustBe Some("Additional identifier")
+      goodsShipment.consignment.flatMap(_.goodsLocation).flatMap(_.typeCode) mustBe Some("T")
+      goodsShipment.consignment.flatMap(_.goodsLocation).flatMap(_.address) must be(defined)
+      goodsShipment.consignment.flatMap(_.departureTransportMeans).flatMap(_.id) must be(defined)
+      goodsShipment.consignment.flatMap(_.departureTransportMeans).flatMap(_.identificationTypeCode) must be(defined)
       goodsShipment.consignment.flatMap(_.containerCode) mustBe Some("1")
 
       goodsShipment.governmentAgencyGoodsItems.size mustBe 10
