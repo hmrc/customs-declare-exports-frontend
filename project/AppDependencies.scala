2174643bce4624a52d03fc3b3db77d95d40dcfda
Patryk Rudnicki <23506899+PatrykRudnicki@users.noreply.github.com>
2019-04-30 15:18:31 +0100

    CEDS-1140 Upgrade for libraries

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,29 +7,28 @@ object AppDependencies {
   val compile = Seq(
     ws,
     "uk.gov.hmrc" %% "play-conditional-form-mapping" % "0.2.0",
-    "uk.gov.hmrc" %% "play-reactivemongo" % "6.0.0",
-    "uk.gov.hmrc" %% "logback-json-logger" % "4.1.0",
-    "uk.gov.hmrc" %% "govuk-template" % "5.3.0",
-    "uk.gov.hmrc" %% "play-health" % "3.9.0-play-25",
-    "uk.gov.hmrc" %% "play-ui" % "7.22.0",
-    "uk.gov.hmrc" %% "http-caching-client" % "8.0.0",
-    "uk.gov.hmrc" %% "bootstrap-play-25" % "4.6.0",
+    "uk.gov.hmrc" %% "play-reactivemongo" % "6.7.0",
+    "uk.gov.hmrc" %% "logback-json-logger" % "4.6.0",
+    "uk.gov.hmrc" %% "govuk-template" % "5.35.0-play-25",
+    "uk.gov.hmrc" %% "play-health" % "3.14.0-play-25",
+    "uk.gov.hmrc" %% "play-ui" % "7.39.0-play-25",
+    "uk.gov.hmrc" %% "http-caching-client" % "8.3.0",
+    "uk.gov.hmrc" %% "bootstrap-play-25" % "4.11.0",
     "uk.gov.hmrc" %% "play-language" % "3.4.0",
-    "com.thoughtworks.xstream" % "xstream" % "1.4.10",
+    "com.thoughtworks.xstream" % "xstream" % "1.4.11.1",
     "uk.gov.hmrc" %% "wco-dec" % "0.30.0",
-    "ai.x"         %% "play-json-extensions" % "0.9.0"
+    "ai.x" %% "play-json-extensions" % "0.9.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc" %% "hmrctest" % "3.0.0" % "test",
-    "org.scalatest" %% "scalatest" % "3.0.4" % "test",
+    "uk.gov.hmrc" %% "hmrctest" % "3.8.0-play-25" % "test",
+    "org.scalatest" %% "scalatest" % "3.0.5" % "test",
     "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.1" % "test",
     "org.pegdown" % "pegdown" % "1.6.0" % "test",
-    "org.jsoup" % "jsoup" % "1.10.3" % "test",
+    "org.jsoup" % "jsoup" % "1.11.3" % "test",
     "com.typesafe.play" %% "play-test" % PlayVersion.current % "test",
-    "org.mockito" % "mockito-core" % "2.13.0" % "test",
+    "org.mockito" % "mockito-core" % "2.27.0" % "test",
     "org.scalacheck" %% "scalacheck" % "1.14.0" % "test"
-    
   )
 
   def apply(): Seq[ModuleID] = compile ++ test
