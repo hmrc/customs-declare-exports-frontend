42fadfdbec5004419f1aa428e396b987ef8526bd
Ed Wilson <4864094+ed0906@users.noreply.github.com>
2019-06-25 15:19:31 +0100

    Update Assets Version in go.sh

--- go.sh
+++ go.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sm --start ASSETS_FRONTEND -f -r 4.3.1 && sm --start CUSTOMS_DECLARE_EXPORTS_FRONTEND_DEPS -f && sbt run
+sm --start ASSETS_FRONTEND -f -r 3.3.2 && sm --start CUSTOMS_DECLARE_EXPORTS_FRONTEND_DEPS -f && sbt run
