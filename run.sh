08d6803830367ac69e465e86f76c9042fc243b8f
Benjamin Parker <benjamin@needstyping.com>
2019-06-28 11:01:31 +0100

    Put assets frontend explicit versioning back to ensure no styling fail

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sm --start CUSTOMS_DECLARE_EXPORTS_FRONTEND_DEPS -f && sbt run
+sm --start ASSETS_FRONTEND -f -r3.3.2 && sm --start CUSTOMS_DECLARE_EXPORTS_FRONTEND_DEPS -f && sbt run
