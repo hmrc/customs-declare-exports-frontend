7356f91261e6df2defb7b8700c15dfbac519c2e4
Patryk Rudnicki <23506899+PatrykRudnicki@users.noreply.github.com>
2019-08-06 15:16:53 +0100

    CEDS-1249 Migrate for DeclarantDetailsControllerSpec to be unit

---
+++ test/unit/controllers/declaration/DeclarantDetailsControllerSpec.scala
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package unit.controllers.declaration
+
+import controllers.declaration.DeclarantDetailsController
+import forms.Choice.AllowedChoiceValues.SupplementaryDec
+import forms.declaration.{DeclarantDetails, EntityDetails}
+import play.api.libs.json.Json
+import play.api.test.Helpers._
+import unit.base.ControllerSpec
+import views.html.declaration.declarant_details
+
+class DeclarantDetailsControllerSpec extends ControllerSpec {
+
+  trait SetUp {
+    val declarantDetailsPage = new declarant_details(mainTemplate)
+
+    val controller = new DeclarantDetailsController(
+      mockAuthAction,
+      mockJourneyAction,
+      mockExportsCacheService,
+      stubMessagesControllerComponents(),
+      declarantDetailsPage
+    )(ec)
+
+    authorizedUser()
+    withNewCaching(aCacheModel(withChoice(SupplementaryDec)))
+  }
+
+  "Declarant Details Controller" should {
+
+    "return 200 (OK)" when {
+
+      "display page method is invoked and cache is empty" in new SetUp {
+
+        val result = controller.displayForm()(getRequest())
+
+        status(result) must be(OK)
+      }
+
+      "display page method is invoked and cache is not empty" in new SetUp {
+
+        withNewCaching(aCacheModel(withDeclarantDetails()))
+
+        val result = controller.displayForm()(getRequest())
+
+        status(result) must be(OK)
+      }
+    }
+
+    "return 400 (BAD_REQUEST)" when {
+
+      "form contains incorrect values" in new SetUp {
+
+        val incorrectForm = Json.toJson(DeclarantDetails(EntityDetails(None, None)))
+
+        val result = controller.saveAddress()(postRequest(incorrectForm))
+
+        status(result) must be(BAD_REQUEST)
+      }
+    }
+
+    "return 303 (SEE_OTHER)" in new SetUp {
+
+      val correctForm = Json.toJson(DeclarantDetails(EntityDetails(Some("12345"), None)))
+
+      val result = controller.saveAddress()(postRequest(correctForm))
+
+      status(result) must be(SEE_OTHER)
+    }
+  }
+}
