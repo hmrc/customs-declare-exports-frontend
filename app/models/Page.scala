0f42dfe456feba9536f7966c2c24aec7a6d9f610
Ed Wilson <4864094+ed0906@users.noreply.github.com>
2019-08-14 09:12:52 +0100

    CEDS-1478 Pagination Review Comments

--- app/models/Page.scala
+++ app/models/Page.scala
@@ -3,24 +3,23 @@ package models
 import play.api.libs.json.{Json, OFormat}
 import play.api.mvc.QueryStringBindable
 
-case class Page(index: Int = 1, size: Int = 50)
+case class Page(index: Int = Page.DEFAULT_INDEX, size: Int = Page.DEFAULT_SIZE)
 
 object Page {
+  val DEFAULT_INDEX = 1
+  val DEFAULT_SIZE = 50
+
   implicit val format: OFormat[Page] = Json.format[Page]
-  implicit def bindable(implicit intBinder: QueryStringBindable[Int]): QueryStringBindable[Page] =
+  implicit val bindable: QueryStringBindable[Page] =
     new QueryStringBindable[Page] {
+      private val intBinder = implicitly[QueryStringBindable[Int]]
       private def queryParamIndex(key: String) = key + "-index"
       private def queryParamSize(key: String) = key + "-size"
 
       override def bind(key: String, params: Map[String, Seq[String]]): Option[Either[String, Page]] = {
-        val index: Option[Int] = params.get(queryParamIndex(key)).flatMap(_.headOption).map(_.toInt)
-        val size: Option[Int] = params.get(queryParamSize(key)).flatMap(_.headOption).map(_.toInt)
-        (index, size) match {
-          case (Some(i), Some(s)) => Some(Right(Page(index = i, size = s)))
-          case (Some(i), None)    => Some(Right(Page(index = i)))
-          case (None, Some(s))    => Some(Right(Page(size = s)))
-          case _                  => Some(Right(Page()))
-        }
+        val index = params.get(queryParamIndex(key)).flatMap(_.headOption).map(_.toInt).getOrElse(DEFAULT_INDEX)
+        val size = params.get(queryParamSize(key)).flatMap(_.headOption).map(_.toInt).getOrElse(DEFAULT_SIZE)
+        Some(Right(Page(index, size)))
       }
 
       override def unbind(key: String, page: Page): String =
