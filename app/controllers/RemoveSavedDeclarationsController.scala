ed379ae812e657ad6a30a292947106a5f8663474
Paulo Monteiro <paulo.from.portugal@gmail.com>
2019-08-27 15:38:03 +0100

    CEDS-1515 Allow deletion of a Draft Declaration

---
+++ app/controllers/RemoveSavedDeclarationsController.scala
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import config.AppConfig
+import connectors.CustomsDeclareExportsConnector
+import controllers.actions.AuthAction
+import forms.RemoveDraftDeclaration.form
+import javax.inject.Inject
+import play.api.i18n.I18nSupport
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.play.bootstrap.controller.FrontendController
+import views.html.remove_declaration
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class RemoveSavedDeclarationsController @Inject()(
+  authenticate: AuthAction,
+  customsDeclareExportsConnector: CustomsDeclareExportsConnector,
+  mcc: MessagesControllerComponents,
+  removeDeclarationPage: remove_declaration,
+  appConfig: AppConfig
+)(implicit ec: ExecutionContext)
+    extends FrontendController(mcc) with I18nSupport {
+
+  def displayPage(id: String): Action[AnyContent] = authenticate.async { implicit request =>
+    customsDeclareExportsConnector.findDeclaration(id) flatMap {
+      case Some(declaration) => Future.successful(Ok(removeDeclarationPage(declaration, form)))
+      case _                 => Future.successful(Redirect(controllers.routes.SavedDeclarationsController.displayDeclarations()))
+    }
+  }
+
+  def removeDeclaration(id: String): Action[AnyContent] = authenticate.async { implicit request =>
+    val removeAction = form.bindFromRequest()
+
+    removeAction
+      .fold(
+        formWithErrors =>
+          customsDeclareExportsConnector.findDeclaration(id) flatMap {
+            case Some(declaration) => Future.successful(BadRequest(removeDeclarationPage(declaration, formWithErrors)))
+            case _                 => Future.successful(Redirect(controllers.routes.SavedDeclarationsController.displayDeclarations()))
+        },
+        validAction =>
+          if (validAction.remove)
+            customsDeclareExportsConnector
+              .deleteDraftDeclaration(id)
+              .map(_ => Redirect(controllers.routes.SavedDeclarationsController.displayDeclarations()))
+          else Future.successful(Redirect(controllers.routes.SavedDeclarationsController.displayDeclarations()))
+      )
+  }
+}
