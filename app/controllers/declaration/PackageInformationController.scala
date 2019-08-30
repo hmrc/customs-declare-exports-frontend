565fa460aa7137f0d92e0db912c9106e7a9307da
Ed Wilson <4864094+ed0906@users.noreply.github.com>
2019-08-30 13:41:05 +0100

    CEDS-1570 Ensure Package Information uses Navigator to continue (#485)
    * CEDS-1570 Ensure Package Information uses Navigator to continue
    
    * CEDS-1570 Scalafmt

--- app/controllers/declaration/PackageInformationController.scala
+++ app/controllers/declaration/PackageInformationController.scala
@@ -100,7 +100,10 @@ class PackageInformationController @Inject()(
               )
           else
             Future
-              .successful(Redirect(controllers.declaration.routes.CommodityMeasureController.displayPage(mode, itemId)))
+              .successful(
+                navigator
+                  .continueTo(controllers.declaration.routes.CommodityMeasureController.displayPage(mode, itemId))
+            )
       )
 
   private def addItem(
