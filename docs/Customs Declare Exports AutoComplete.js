7b8000b7a52032fe3c576afa6c414fa61712b003
Steve Sugden <46934286+SteveSugden@users.noreply.github.com>
2019-09-09 12:00:47 +0100

    BAU - update version no. of script

--- docs/Customs Declare Exports AutoComplete.js
+++ docs/Customs Declare Exports AutoComplete.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Customs Declare Exports AutoComplete
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      0.5
 // @description  try to take over the world!
 // @author       You
 // @match        http*://*/customs-declare-exports*
