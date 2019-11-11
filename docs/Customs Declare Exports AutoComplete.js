e25cfb1e7c7c9d66206536fe35e23f182ebc8dbe
Paulo Monteiro <paulo.from.portugal@gmail.com>
2019-11-11 13:04:56 +0000

    Update Customs Declare Exports AutoComplete.js

--- docs/Customs Declare Exports AutoComplete.js
+++ docs/Customs Declare Exports AutoComplete.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Customs Declare Exports AutoComplete
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.3
 // @description  try to take over the world!
 // @author       You
 // @match        http*://*/customs-declare-exports*
