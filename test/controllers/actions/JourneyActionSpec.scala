ff7ebc02efeda3e03f945dfa3d71a50afc1b2eaa
Ed Wilson <4864094+ed0906@users.noreply.github.com>
2019-12-09 08:59:59 +0000

    CEDS Allow JourneyAction to be restricted to Dec Type with e.g. action(Standard) (#669)

---
+++ test/controllers/actions/JourneyActionSpec.scala
@@ -0,0 +1,71 @@
+package controllers.actions
+
+import models.requests.{AuthenticatedRequest, JourneyRequest}
+import models.{IdentityData, SignedInUser}
+import org.mockito.ArgumentCaptor
+import org.mockito.ArgumentMatchers._
+import org.mockito.BDDMockito._
+import org.mockito.Mockito._
+import org.scalatest.mockito.MockitoSugar
+import org.scalatest.{BeforeAndAfterEach, MustMatchers, WordSpec}
+import play.api.mvc.{AnyContentAsEmpty, Result, Results}
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import services.cache.{ExportsCacheService, ExportsDeclarationBuilder}
+import uk.gov.hmrc.auth.core.Enrolments
+import uk.gov.hmrc.http.HeaderCarrier
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+class JourneyActionSpec extends WordSpec with MustMatchers with MockitoSugar with BeforeAndAfterEach with ExportsDeclarationBuilder {
+
+  private implicit val hc: HeaderCarrier = mock[HeaderCarrier]
+  private val cache = mock[ExportsCacheService]
+  private val block = mock[JourneyRequest[_] => Future[Result]]
+  private val user = SignedInUser("eori", Enrolments(Set.empty), IdentityData())
+  private val declaration = aDeclaration()
+  private val refiner = new JourneyAction(cache)
+
+  private def request(declarationId: Option[String]): AuthenticatedRequest[AnyContentAsEmpty.type] = declarationId match {
+    case Some(id) =>
+      new AuthenticatedRequest(FakeRequest().withSession("declarationId" -> id), user)
+    case None => new AuthenticatedRequest(FakeRequest(), user)
+  }
+
+  override def afterEach(): Unit = {
+    reset(cache, block)
+    super.afterEach()
+  }
+  "refine" should {
+    "permit request" when {
+      "answers found" in {
+        given(block.apply(any())).willReturn(Future.successful(Results.Ok))
+        given(cache.get(refEq("id"))(any[HeaderCarrier])).willReturn(Future.successful(Some(declaration)))
+
+        await(refiner.invokeBlock(request(Some("id")), block)) mustBe Results.Ok
+
+        val result = theRequestBuilt
+        result.cacheModel mustBe declaration
+      }
+
+      def theRequestBuilt: JourneyRequest[_] = {
+        val captor = ArgumentCaptor.forClass(classOf[JourneyRequest[_]])
+        verify(block).apply(captor.capture())
+        captor.getValue
+      }
+    }
+
+    "block request" when {
+      "answers not found" in {
+        given(cache.get(refEq("id"))(any[HeaderCarrier])).willReturn(Future.successful(None))
+
+        await(refiner.invokeBlock(request(Some("id")), block)) mustBe Results.Redirect(controllers.routes.StartController.displayStartPage())
+      }
+
+      "id not found" in {
+        await(refiner.invokeBlock(request(None), block)) mustBe Results.Redirect(controllers.routes.StartController.displayStartPage())
+      }
+    }
+  }
+}
