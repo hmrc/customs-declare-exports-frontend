7a1f5d8a98639886a08d2d1886d89e66ffddba94
Lukasz Dubiel <974784+bambuchaAdm@users.noreply.github.com>
2019-12-13 14:44:56 +0000

    CEDS-2036 Tests for reads-writes

--- test/connectors/exchange/ExportsDeclarationExchangeSpec.scala
+++ test/connectors/exchange/ExportsDeclarationExchangeSpec.scala
@@ -26,6 +26,7 @@ import models.{DeclarationStatus, DeclarationType, ExportsDeclaration}
 import org.mockito.Mockito.when
 import org.scalatest.{Matchers, WordSpec}
 import org.scalatestplus.mockito.MockitoSugar
+import play.api.libs.json.{JsSuccess, Json}
 import services.cache.ExportsDeclarationBuilder
 
 class ExportsDeclarationExchangeSpec extends WordSpec with Matchers with ExportsDeclarationBuilder with MockitoSugar {
@@ -100,5 +101,398 @@ class ExportsDeclarationExchangeSpec extends WordSpec with Matchers with Exports
     "map from ExportsDeclaration" in {
       ExportsDeclarationExchange(declaration) shouldBe request
     }
+
+    "bindable by version1 of payload" in {
+      Json
+        .parse(ExportsDeclarationExchangeSpec.declarationVersion1)
+        .validate[ExportsDeclarationExchange] shouldBe a[JsSuccess[_]]
+    }
+
+    "bindable by version2 of paylaod" in {
+      Json
+        .parse(ExportsDeclarationExchangeSpec.declarationVersion2)
+        .validate[ExportsDeclarationExchange] shouldBe a[JsSuccess[_]]
+    }
+
+    "serialize to form that could be bindable" in {
+      val declaration = aDeclaration()
+      Json.toJson(ExportsDeclarationExchange(declaration)).validate[ExportsDeclarationExchange] shouldBe a[JsSuccess[_]]
+    }
   }
+}
+
+object ExportsDeclarationExchangeSpec {
+  val declarationVersion1: String =
+    """{
+      |  "id": "6f31582e-bfd5-4b27-90be-2dca6e236b20",
+      |  "eori": "GB7172755078551",
+      |  "status": "DRAFT",
+      |  "createdDateTime": "2019-12-10T15:52:32.681Z",
+      |  "updatedDateTime": "2019-12-10T15:53:13.697Z",
+      |  "type": "STANDARD",
+      |  "dispatchLocation": {
+      |    "dispatchLocation": "EX"
+      |  },
+      |  "additionalDeclarationType": "D",
+      |  "consignmentReferences": {
+      |    "ducr": {
+      |      "ducr": "8GB123451068100-101SHIP1"
+      |    },
+      |    "lrn": "QSLRN7285100"
+      |  },
+      |  "departureTransport": {
+      |    "borderModeOfTransportCode": "1",
+      |    "meansOfTransportOnDepartureType": "11",
+      |    "meansOfTransportOnDepartureIDNumber": "SHIP1"
+      |  },
+      |  "borderTransport": {
+      |    "meansOfTransportCrossingTheBorderNationality": "United Kingdom",
+      |    "meansOfTransportCrossingTheBorderType": "11",
+      |    "meansOfTransportCrossingTheBorderIDNumber": "Boaty McBoatface"
+      |  },
+      |  "transportInformation": {
+      |    "transportPayment": {
+      |      "paymentMethod": "H"
+      |    },
+      |    "containers": [
+      |      {
+      |        "id": "123456",
+      |        "seals": []
+      |      }
+      |    ]
+      |  },
+      |  "parties": {
+      |    "exporterDetails": {
+      |      "details": {
+      |        "eori": "GB717572504502801"
+      |      }
+      |    },
+      |    "consigneeDetails": {
+      |      "details": {
+      |        "address": {
+      |          "fullName": "Bags Export",
+      |          "addressLine": "1 Bags Avenue",
+      |          "townOrCity": "New York",
+      |          "postCode": "NA",
+      |          "country": "United States of America"
+      |        }
+      |      }
+      |    },
+      |    "declarantDetails": {
+      |      "details": {
+      |        "eori": "GB717572504502811"
+      |      }
+      |    },
+      |    "representativeDetails": {
+      |      "details": {
+      |        "eori": "GB717572504502809"
+      |      },
+      |      "statusCode": "3"
+      |    },
+      |    "declarationHoldersData": {
+      |      "holders": [
+      |        {
+      |          "authorisationTypeCode": "AEOC",
+      |          "eori": "GB717572504502811"
+      |        }
+      |      ]
+      |    },
+      |    "carrierDetails": {
+      |      "details": {
+      |        "address": {
+      |          "fullName": "XYZ Carrier",
+      |          "addressLine": "School Road",
+      |          "townOrCity": "London",
+      |          "postCode": "WS1 2AB",
+      |          "country": "United Kingdom"
+      |        }
+      |      }
+      |    }
+      |  },
+      |  "locations": {
+      |    "originationCountry": "GB",
+      |    "destinationCountry": "DE",
+      |    "hasRoutingCountries": true,
+      |    "routingCountries": [
+      |      "FR"
+      |    ],
+      |    "goodsLocation": {
+      |      "country": "United Kingdom",
+      |      "typeOfLocation": "A",
+      |      "qualifierOfIdentification": "U",
+      |      "identificationOfLocation": "FXTFXTFXT",
+      |      "additionalIdentifier": "123"
+      |    },
+      |    "officeOfExit": {
+      |      "officeId": "GB000054",
+      |      "circumstancesCode": "No"
+      |    },
+      |    "supervisingCustomsOffice": {
+      |      "supervisingCustomsOffice": "GBLBA001"
+      |    },
+      |    "warehouseIdentification": {},
+      |    "inlandModeOfTransportCode": {
+      |      "inlandModeOfTransportCode": "1"
+      |    }
+      |  },
+      |  "items": [
+      |    {
+      |      "id": "b12agbfd",
+      |      "sequenceId": 1,
+      |      "procedureCodes": {
+      |        "procedureCode": "1040",
+      |        "additionalProcedureCodes": [
+      |          "000"
+      |        ]
+      |      },
+      |      "fiscalInformation": {
+      |        "onwardSupplyRelief": "No"
+      |      },
+      |      "statisticalValue": {
+      |        "statisticalValue": "1000"
+      |      },
+      |      "commodityDetails": {
+      |        "combinedNomenclatureCode": "46021910",
+      |        "descriptionOfGoods": "Straw for bottles"
+      |      },
+      |      "dangerousGoodsCode": {
+      |        "dangerousGoodsCode": "1234"
+      |      },
+      |      "cusCode": {
+      |        "cusCode": "12345678"
+      |      },
+      |      "taricCodes": [],
+      |      "nactCodes": [],
+      |      "packageInformation": [
+      |        {
+      |          "typesOfPackages": "PK",
+      |          "numberOfPackages": 10,
+      |          "shippingMarks": "RICH123"
+      |        }
+      |      ],
+      |      "commodityMeasure": {
+      |        "supplementaryUnits": "10",
+      |        "netMass": "500",
+      |        "grossMass": "700"
+      |      },
+      |      "additionalInformation": {
+      |        "items": [
+      |          {
+      |            "code": "00400",
+      |            "description": "EXPORTER"
+      |          }
+      |        ]
+      |      },
+      |      "documentsProducedData": {
+      |        "documents": [
+      |          {
+      |            "documentTypeCode": "C501",
+      |            "documentIdentifier": "GBAEOC717572504502811"
+      |          }
+      |        ]
+      |      }
+      |    }
+      |  ],
+      |  "totalNumberOfItems": {
+      |    "totalAmountInvoiced": "56764",
+      |    "exchangeRate": "1.49",
+      |    "totalPackage": "1"
+      |  },
+      |  "previousDocuments": {
+      |    "documents": [
+      |      {
+      |        "documentCategory": "Y",
+      |        "documentType": "IF3",
+      |        "documentReference": "101SHIP2"
+      |      }
+      |    ]
+      |  },
+      |  "natureOfTransaction": {
+      |    "natureType": "1"
+      |  }
+      |}""".stripMargin
+
+  val declarationVersion2: String =
+    """{
+      |  "id": "6f31582e-bfd5-4b27-90be-2dca6e236b20",
+      |  "eori": "GB7172755078551",
+      |  "status": "DRAFT",
+      |  "createdDateTime": "2019-12-10T15:52:32.681Z",
+      |  "updatedDateTime": "2019-12-10T15:53:13.697Z",
+      |  "type": "STANDARD",
+      |  "dispatchLocation": {
+      |    "dispatchLocation": "EX"
+      |  },
+      |  "additionalDeclarationType": "D",
+      |  "consignmentReferences": {
+      |    "ducr": {
+      |      "ducr": "8GB123451068100-101SHIP1"
+      |    },
+      |    "lrn": "QSLRN7285100"
+      |  },
+      |  "transport": {
+      |    "transportPayment": {
+      |      "paymentMethod": "H"
+      |    },
+      |    "containers": [
+      |      {
+      |        "id": "123456",
+      |        "seals": []
+      |      }
+      |    ],
+      |    "inlandModeOfTransportCode": "1",
+      |    "meansOfTransportCrossingTheBorderNationality": "United Kingdom",
+      |    "meansOfTransportCrossingTheBorderType": "11",
+      |    "meansOfTransportCrossingTheBorderIDNumber": "Boaty McBoatface",
+      |    "borderModeOfTransportCode": "1",
+      |    "meansOfTransportOnDepartureType": "11",
+      |    "meansOfTransportOnDepartureIDNumber": "SHIP1"
+      |  },
+      |  "parties": {
+      |    "exporterDetails": {
+      |      "details": {
+      |        "eori": "GB717572504502801"
+      |      }
+      |    },
+      |    "consigneeDetails": {
+      |      "details": {
+      |        "address": {
+      |          "fullName": "Bags Export",
+      |          "addressLine": "1 Bags Avenue",
+      |          "townOrCity": "New York",
+      |          "postCode": "NA",
+      |          "country": "United States of America"
+      |        }
+      |      }
+      |    },
+      |    "declarantDetails": {
+      |      "details": {
+      |        "eori": "GB717572504502811"
+      |      }
+      |    },
+      |    "representativeDetails": {
+      |      "details": {
+      |        "eori": "GB717572504502809"
+      |      },
+      |      "statusCode": "3"
+      |    },
+      |    "declarationHoldersData": {
+      |      "holders": [
+      |        {
+      |          "authorisationTypeCode": "AEOC",
+      |          "eori": "GB717572504502811"
+      |        }
+      |      ]
+      |    },
+      |    "carrierDetails": {
+      |      "details": {
+      |        "address": {
+      |          "fullName": "XYZ Carrier",
+      |          "addressLine": "School Road",
+      |          "townOrCity": "London",
+      |          "postCode": "WS1 2AB",
+      |          "country": "United Kingdom"
+      |        }
+      |      }
+      |    }
+      |  },
+      |  "locations": {
+      |    "originationCountry": "GB",
+      |    "destinationCountry": "DE",
+      |    "hasRoutingCountries": true,
+      |    "routingCountries": [
+      |      "FR"
+      |    ],
+      |    "goodsLocation": {
+      |      "country": "United Kingdom",
+      |      "typeOfLocation": "A",
+      |      "qualifierOfIdentification": "U",
+      |      "identificationOfLocation": "FXTFXTFXT",
+      |      "additionalIdentifier": "123"
+      |    },
+      |    "officeOfExit": {
+      |      "officeId": "GB000054",
+      |      "circumstancesCode": "No"
+      |    },
+      |    "supervisingCustomsOffice": {
+      |      "supervisingCustomsOffice": "GBLBA001"
+      |    },
+      |    "warehouseIdentification": {}
+      |  },
+      |  "items": [
+      |    {
+      |      "id": "b12agbfd",
+      |      "sequenceId": 1,
+      |      "procedureCodes": {
+      |        "procedureCode": "1040",
+      |        "additionalProcedureCodes": [
+      |          "000"
+      |        ]
+      |      },
+      |      "fiscalInformation": {
+      |        "onwardSupplyRelief": "No"
+      |      },
+      |      "statisticalValue": {
+      |        "statisticalValue": "1000"
+      |      },
+      |      "commodityDetails": {
+      |        "combinedNomenclatureCode": "46021910",
+      |        "descriptionOfGoods": "Straw for bottles"
+      |      },
+      |      "dangerousGoodsCode": {
+      |        "dangerousGoodsCode": "1234"
+      |      },
+      |      "cusCode": {
+      |        "cusCode": "12345678"
+      |      },
+      |      "taricCodes": [],
+      |      "nactCodes": [],
+      |      "packageInformation": [
+      |        {
+      |          "typesOfPackages": "PK",
+      |          "numberOfPackages": 10,
+      |          "shippingMarks": "RICH123"
+      |        }
+      |      ],
+      |      "commodityMeasure": {
+      |        "supplementaryUnits": "10",
+      |        "netMass": "500",
+      |        "grossMass": "700"
+      |      },
+      |      "additionalInformation": {
+      |        "items": [
+      |          {
+      |            "code": "00400",
+      |            "description": "EXPORTER"
+      |          }
+      |        ]
+      |      },
+      |      "documentsProducedData": {
+      |        "documents": [
+      |          {
+      |            "documentTypeCode": "C501",
+      |            "documentIdentifier": "GBAEOC717572504502811"
+      |          }
+      |        ]
+      |      }
+      |    }
+      |  ],
+      |  "totalNumberOfItems": {
+      |    "totalAmountInvoiced": "56764",
+      |    "exchangeRate": "1.49",
+      |    "totalPackage": "1"
+      |  },
+      |  "previousDocuments": {
+      |    "documents": [
+      |      {
+      |        "documentCategory": "Y",
+      |        "documentType": "IF3",
+      |        "documentReference": "101SHIP2"
+      |      }
+      |    ]
+      |  },
+      |  "natureOfTransaction": {
+      |    "natureType": "1"
+      |  }
+      |}""".stripMargin
 }
