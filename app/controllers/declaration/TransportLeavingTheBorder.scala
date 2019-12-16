f6b88eaaed8a03816aae14904202b5f5e8a37059
Lukasz Dubiel <974784+bambuchaAdm@users.noreply.github.com>
2019-12-16 13:55:45 +0000

    CEDS-2036 Add missing files

---
+++ app/controllers/declaration/TransportLeavingTheBorder.scala
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.declaration
+
+import controllers.actions.{AuthAction, JourneyAction}
+import controllers.navigation.Navigator
+import forms.declaration.ModeOfTransportCodes
+import javax.inject.Inject
+import models.DeclarationType.DeclarationType
+import models.requests.JourneyRequest
+import models.{DeclarationType, Mode}
+import play.api.i18n.I18nSupport
+import play.api.mvc.{AnyContent, Call, MessagesControllerComponents, Result}
+import services.cache.ExportsCacheService
+import uk.gov.hmrc.play.bootstrap.controller.FrontendController
+import views.html.declaration.transport_leaving_the_border
+
+import scala.concurrent.ExecutionContext
+
+class TransportLeavingTheBorder @Inject()(
+  authenticate: AuthAction,
+  journeyType: JourneyAction,
+  override val exportsCacheService: ExportsCacheService,
+  navigator: Navigator,
+  mcc: MessagesControllerComponents,
+  transportAtBorder: transport_leaving_the_border
+)(implicit ec: ExecutionContext)
+    extends FrontendController(mcc) with I18nSupport with ModelCacheable {
+
+  private val validTypes = Seq(DeclarationType.STANDARD, DeclarationType.SUPPLEMENTARY, DeclarationType.CLEARANCE)
+
+  def displayPage(mode: Mode) = (authenticate andThen journeyType(validTypes)) { implicit request =>
+    val form = ModeOfTransportCodes.form
+    request.cacheModel.transport.borderModeOfTransportCode.flatMap(ModeOfTransportCodes.apply) match {
+      case Some(data) => Ok(transportAtBorder(form.fill(data), mode))
+      case _          => Ok(transportAtBorder(form, mode))
+    }
+  }
+
+  def submitForm(mode: Mode) = (authenticate andThen journeyType(validTypes)).async { implicit request =>
+    ModeOfTransportCodes
+      .form
+      .bindFromRequest()
+      .fold(
+        errors => ???,
+        code => updateExportsDeclarationSyncDirect(_.updateTransportLeavingBorder(code)).map { _ =>
+          navigator.continueTo(nextPage(mode))
+        }
+      )
+  }
+
+  def nextPage(mode: Mode)(implicit request: JourneyRequest[AnyContent]): Call =  request.declarationType match {
+    case DeclarationType.CLEARANCE =>
+      controllers.declaration.routes.TransportContainerController.displayContainerSummary(mode)
+    case DeclarationType.STANDARD | DeclarationType.SUPPLEMENTARY =>
+      controllers.declaration.routes.DepartureTransportController.displayPage(mode)
+  }
+}
---
+++ app/forms/declaration/ModeOfTransportCodes.scala
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package forms.declaration
+
+import forms.DeclarationPage
+import play.api.data.format.{Formats, Formatter}
+import play.api.data.{Form, FormError, Forms}
+import play.api.libs.json.{JsString, JsonValidationError, Reads, Writes}
+
+sealed abstract class ModeOfTransportCodes(val value: String)
+
+object ModeOfTransportCodes extends DeclarationPage {
+
+  case object Maritime extends ModeOfTransportCodes("1")
+  case object Rail extends ModeOfTransportCodes("2")
+  case object Road extends ModeOfTransportCodes("3")
+  case object Air extends ModeOfTransportCodes("4")
+  case object PostalConsignment extends ModeOfTransportCodes("5")
+  case object FixedTransportInstallations extends ModeOfTransportCodes("7")
+  case object InlandWaterway extends ModeOfTransportCodes("8")
+  case object Unknown extends ModeOfTransportCodes("9")
+
+  val allowedModeOfTransportCodes: Set[ModeOfTransportCodes] =
+    Set(Maritime, Rail, Road, Air, PostalConsignment, FixedTransportInstallations, InlandWaterway, Unknown)
+
+  private val reverseLookup: Map[String, ModeOfTransportCodes] = allowedModeOfTransportCodes.map(entry => entry.value -> entry).toMap
+
+  def apply(code: String): Option[ModeOfTransportCodes] = reverseLookup.get(code)
+
+  def formatter(errorKey: String): Formatter[ModeOfTransportCodes] = new Formatter[ModeOfTransportCodes] {
+    override def bind(key: String, data: Map[String, String]): Either[Seq[FormError], ModeOfTransportCodes] =
+      Formats.stringFormat
+        .bind(key, data)
+        .right
+        .flatMap(value => reverseLookup.get(value).map(Right.apply).getOrElse(Left(Seq(FormError.apply(key, errorKey)))))
+
+    override def unbind(key: String, code: ModeOfTransportCodes): Map[String, String] =
+      Map(key -> code.value)
+  }
+
+  implicit val reads: Reads[ModeOfTransportCodes] = Reads.StringReads.collect(JsonValidationError("error.unknown"))(reverseLookup)
+
+  implicit val writes: Writes[ModeOfTransportCodes] = Writes(code => JsString(code.value))
+
+  import Forms._
+
+  private val mapping = Forms.single("code" -> of(formatter("error.unknown")))
+
+  val form: Form[ModeOfTransportCodes] = Form(mapping)
+}
