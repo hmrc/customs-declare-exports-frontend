469f2443f0e3563a5f16be5c5c6b0584b7ade670
pmonteiro <paulo.from.portugal@gmail.com>
2020-03-24 13:34:27 +0000

    CEDS-2237 - Improve 'Other Parties' page
    Improve the design of the 'Other Parties' page so that it is better understood by declarants. 
    
     

--- app/forms/declaration/DeclarationAdditionalActors.scala
+++ app/forms/declaration/DeclarationAdditionalActors.scala
@@ -21,8 +21,8 @@ import forms.common.Eori
 import forms.declaration.DeclarationAdditionalActors.PartyType
 import play.api.data.Forms.{optional, text}
 import play.api.data.{Form, Forms}
-import play.api.libs.json.{JsValue, Json}
-import uk.gov.voa.play.form.ConditionalMappings.mandatoryIfNot
+import play.api.libs.json.{Format, JsValue, Json, OFormat, Reads}
+import uk.gov.voa.play.form.ConditionalMappings._
 import utils.validators.forms.FieldValidator._
 
 case class DeclarationAdditionalActors(eori: Option[Eori], partyType: Option[String]) {
@@ -36,7 +36,7 @@ object DeclarationAdditionalActors extends DeclarationPage {
 
   def fromJsonString(value: String): Option[DeclarationAdditionalActors] = Json.fromJson(Json.parse(value)).asOpt
 
-  implicit val format = Json.format[DeclarationAdditionalActors]
+  implicit val format: Format[DeclarationAdditionalActors] = Json.format[DeclarationAdditionalActors]
 
   private val allowedPartyTypes =
     Set(PartyType.Consolidator, PartyType.Manufacturer, PartyType.FreightForwarder, PartyType.WarehouseKeeper)
@@ -44,15 +44,31 @@ object DeclarationAdditionalActors extends DeclarationPage {
   val formId = "DeclarationAdditionalActors"
 
   val mapping = Forms.mapping(
-    "eori" -> optional(Eori.mapping("supplementary")),
-    "partyType" -> mandatoryIfNot(
-      "eori",
-      "",
-      optional(text().verifying("supplementary.partyType.error", isContainedIn(allowedPartyTypes)))
-        .verifying("supplementary.partyType.empty", _.isDefined)
-        .transform[String](optValue => optValue.getOrElse(""), party => Some(party))
-    )
-  )(DeclarationAdditionalActors.apply)(DeclarationAdditionalActors.unapply)
+    "eoriCS" -> eoriMappingFor(PartyType.Consolidator),
+    "eoriMF" -> eoriMappingFor(PartyType.Manufacturer),
+    "eoriFW" -> eoriMappingFor(PartyType.FreightForwarder),
+    "eoriWH" -> eoriMappingFor(PartyType.WarehouseKeeper),
+    "partyType" -> mandatory(text().verifying("supplementary.partyType.error", isContainedIn(allowedPartyTypes + "no")))
+  )(form2Model)(model2Form)
+
+  private def eoriMappingFor(partyType: String) =
+    mandatoryIfEqual("partyType", partyType, Eori.mapping("supplementary"))
+
+  private def form2Model: (Option[Eori], Option[Eori], Option[Eori], Option[Eori], Option[String]) => DeclarationAdditionalActors = {
+    case (eoriCS, eoriMF, eoriFW, eoriWH, partyType) =>
+      val eori = Seq(eoriCS, eoriMF, eoriFW, eoriWH).zip(allowedPartyTypes).find(z => partyType.contains(z._2)).flatMap(_._1)
+      DeclarationAdditionalActors(eori, partyType)
+  }
+
+  private def model2Form: DeclarationAdditionalActors => Option[(Option[Eori], Option[Eori], Option[Eori], Option[Eori], Option[String])] = { model =>
+    model.partyType match {
+      case Some(PartyType.Consolidator)     => Option(model.eori, None, None, None, model.partyType)
+      case Some(PartyType.Manufacturer)     => Option(None, model.eori, None, None, model.partyType)
+      case Some(PartyType.FreightForwarder) => Option(None, None, model.eori, None, model.partyType)
+      case Some(PartyType.WarehouseKeeper)  => Option(None, None, None, model.eori, model.partyType)
+      case _                                => Option(None, None, None, None, model.partyType)
+    }
+  }
 
   def form(): Form[DeclarationAdditionalActors] = Form(mapping)
 
