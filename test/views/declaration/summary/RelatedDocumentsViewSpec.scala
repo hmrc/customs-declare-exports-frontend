05aa5bff18d9fae692685c0d6473bc30f74d81b6
“alsahidS” <alsahid.simms@digital.hmrc.gov.uk>
2020-04-02 11:06:37 +0100

    CEDS-2230-summary -Posting updates

--- test/views/declaration/summary/RelatedDocumentsViewSpec.scala
+++ test/views/declaration/summary/RelatedDocumentsViewSpec.scala
@@ -16,13 +16,15 @@
 
 package views.declaration.summary
 
+import base.Injector
 import forms.declaration.Document
 import models.Mode
 import services.cache.ExportsTestData
 import views.declaration.spec.UnitViewSpec
-import views.html.declaration.summary.related_documents
+import views.html.declaration.summary.related_documents_gds
 
-class RelatedDocumentsViewSpec extends UnitViewSpec with ExportsTestData {
+class RelatedDocumentsViewSpec extends UnitViewSpec with ExportsTestData with Injector {
+  private val section = instanceOf[related_documents_gds]
 
   "Related documents" should {
 
@@ -30,19 +32,30 @@ class RelatedDocumentsViewSpec extends UnitViewSpec with ExportsTestData {
 
       "documents are empty" in {
 
-        val view = related_documents(Mode.Change, Seq.empty)(messages, journeyRequest())
 
-        view.getElementById("previous-documents-label").text() mustBe messages("declaration.summary.transaction.previousDocuments")
-        view.getElementById("previous-documents").text() mustBe messages("site.no")
+        val view = section(Mode.Normal, Seq.empty)(messages, journeyRequest())
+        val row = view.getElementsByClass("previous-documents-row")
 
-        val List(change, accessibleChange) = view.getElementById("previous-documents-change").text().split(" ").toList
+        row must haveSummaryKey(messages("declaration.summary.parties.holders"))
+        row must haveSummaryValue(messages("site.no"))
 
-        change mustBe messages("site.change")
-        accessibleChange mustBe messages("declaration.summary.transaction.previousDocuments.change")
+        row must haveSummaryActionsText("site.change declaration.summary.transaction.previousDocuments.change")
 
-        view.getElementById("previous-documents-change") must haveHref(
-          controllers.declaration.routes.PreviousDocumentsController.displayPage(Mode.Change)
-        )
+        row must haveSummaryActionsHref(controllers.declaration.routes.PreviousDocumentsController.displayPage(Mode.Normal))
+
+//        val view = related_documents_gds(Mode.Change, Seq.empty)(messages, journeyRequest())
+//
+//        view.getElementById("previous-documents-label").text() mustBe messages("declaration.summary.transaction.previousDocuments")
+//        view.getElementById("previous-documents").text() mustBe messages("site.no")
+//
+//        val List(change, accessibleChange) = view.getElementById("previous-documents-change").text().split(" ").toList
+//
+//        change mustBe messages("site.change")
+//        accessibleChange mustBe messages("declaration.summary.transaction.previousDocuments.change")
+//
+//        view.getElementById("previous-documents-change") must haveHref(
+//          controllers.declaration.routes.PreviousDocumentsController.displayPage(Mode.Change)
+//        )
       }
     }
 
@@ -52,33 +65,65 @@ class RelatedDocumentsViewSpec extends UnitViewSpec with ExportsTestData {
 
         val data = Seq(Document("X", "325", "123456", None), Document("X", "271", "654321", None))
 
-        val view = related_documents(Mode.Change, data)(messages, journeyRequest())
+       // val view = section(Mode.Normal, data)(messages, journeyRequest())
 
-        view.getElementById("previous-documents-label").text() mustBe messages("declaration.summary.transaction.previousDocuments")
-        view.getElementById("previous-documents-type").text() mustBe messages("declaration.summary.transaction.previousDocuments.type")
-        view.getElementById("previous-documents-reference").text() mustBe messages("declaration.summary.transaction.previousDocuments.reference")
-        view.getElementById("previous-document-0-type").text() mustBe "Proforma Invoice - 325"
-        view.getElementById("previous-document-0-reference").text() mustBe "123456"
-
-        val List(change1, accessibleChange1) = view.getElementById("previous-document-0-change").text().split(" ").toList
-
-        change1 mustBe messages("site.change")
-        accessibleChange1 mustBe messages("declaration.summary.transaction.previousDocuments.document.change", 0)
-
-        view.getElementById("previous-document-0-change") must haveHref(
-          controllers.declaration.routes.PreviousDocumentsController.displayPage(Mode.Change)
-        )
+//        val view = related_documents(Mode.Change, data)(messages, journeyRequest())
+//
+//        view.getElementById("previous-documents-label").text() mustBe messages("declaration.summary.transaction.previousDocuments")
+//        view.getElementById("previous-documents-type").text() mustBe messages("declaration.summary.transaction.previousDocuments.type")
+//        view.getElementById("previous-documents-reference").text() mustBe messages("declaration.summary.transaction.previousDocuments.reference")
+//        view.getElementById("previous-document-0-type").text() mustBe "Proforma Invoice - 325"
+//        view.getElementById("previous-document-0-reference").text() mustBe "123456"
+//
+//        val List(change1, accessibleChange1) = view.getElementById("previous-document-0-change").text().split(" ").toList
+//
+//        change1 mustBe messages("site.change")
+//        accessibleChange1 mustBe messages("declaration.summary.transaction.previousDocuments.document.change", 0)
+//
+//        view.getElementById("previous-document-0-change") must haveHref(
+//          controllers.declaration.routes.PreviousDocumentsController.displayPage(Mode.Change)
+//        )
         view.getElementById("previous-document-1-type").text() mustBe "Packing List - 271"
         view.getElementById("previous-document-1-reference").text() mustBe "654321"
 
         val List(change2, accessibleChange2) = view.getElementById("previous-document-1-change").text().split(" ").toList
 
-        change2 mustBe messages("site.change")
-        accessibleChange2 mustBe messages("declaration.summary.transaction.previousDocuments.document.change", 1)
+//        change2 mustBe messages("site.change")
+//        accessibleChange2 mustBe messages("declaration.summary.transaction.previousDocuments.document.change", 1)
+//
+//        view.getElementById("previous-document-1-change") must haveHref(
+//          controllers.declaration.routes.PreviousDocumentsController.displayPage(Mode.Change)
+//        )
 
-        view.getElementById("previous-document-1-change") must haveHref(
-          controllers.declaration.routes.PreviousDocumentsController.displayPage(Mode.Change)
-        )
+        val view = section(Mode.Change, data)(messages, journeyRequest())
+        val table = view.getElementById("previous-documents")
+
+        table.getElementsByTag("caption").text() mustBe messages("declaration.summary.transaction.previousDocuments")
+        table.getElementsByClass("govuk-table__header").get(0).text() mustBe messages("declaration.summary.transaction.previousDocuments.type")
+        table.getElementsByClass("govuk-table__header").get(1).text() mustBe messages("declaration.summary.transaction.previousDocuments.reference")
+        table.getElementsByClass("govuk-table__header").get(2).text() mustBe messages("")
+        table.getElementsByClass("govuk-table__header-0-type").text() mustBe messages("Proforma Invoice - 325") //needs changing
+        table.getElementsByClass("govuk-table__header-0-reference").text() mustBe messages("123456") //needs changing
+
+        val List(change1, accessibleChange1) = view.getElementById("previous-document-0-change").text().split(" ").toList
+
+        table mustBe haveSummaryActionsText("site.change declaration.summary.transaction.previousDocuments.document.change",0)
+
+        view.getElementsByClass mustBe  haveSummaryActionsHref(controllers.declaration.routes.PreviousDocumentsController.displayPage(Mode.Change))
+
+
+
+
+
+
+        val row = view.getElementsByClass("previous-documents-row")
+        row must haveSummaryKey(messages("declaration.summary.transaction.previousDocuments"))
+        row must haveSummaryValue("123")
+
+        row must haveSummaryActionsText("site.change declaration.summary.locations.officeOfExit.change")
+
+        row must haveSummaryActionsHref(controllers.declaration.routes.OfficeOfExitController.displayPage(Mode.Change))
+
       }
     }
   }
