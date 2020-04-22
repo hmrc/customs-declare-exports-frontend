518999f6f80ebbac081400c12a3a299eebf6f48e
Steve Sugden <46934286+SteveSugden@users.noreply.github.com>
2020-04-22 08:10:02 +0100

    BAU - fix for NACT code page Quick Submit

--- docs/Customs Declare Exports AutoComplete.js
+++ docs/Customs Declare Exports AutoComplete.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Customs Declare Exports AutoComplete
 // @namespace    http://tampermonkey.net/
-// @version      1.38
+// @version      1.39
 // @description  decs supported: (Std-Frontier A), (Occ-Frontier B), (Smp-Frontier C), (Std-PreLodged D), (Occ-PreLodged E), (Smp-PreLodged F), (Clr-Frontier J), (Clr-PreLodged K), (Sup-SDP Y), (Sup-EIDR Z)
 // @author       You
 // @match        http*://*/customs-declare-exports*
@@ -636,7 +636,7 @@ function taricCodes(){
 
 function nactCodes(){
     if (currentPageIs('/customs-declare-exports/declaration/items/.*/nact-codes')) {
-        document.getElementsByClassName('button')[0].click()
+        document.getElementById('submit').click();
     }
 }
 
