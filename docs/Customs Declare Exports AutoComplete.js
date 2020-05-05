a2b04403886cf9a7070a08b9c11537106308a7ba
Steve Sugden <46934286+SteveSugden@users.noreply.github.com>
2020-05-05 16:25:42 +0100

    CEDS-2401 - remove representative EORI from summary if not asked

--- docs/Customs Declare Exports AutoComplete.js
+++ docs/Customs Declare Exports AutoComplete.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Customs Declare Exports AutoComplete
 // @namespace    http://tampermonkey.net/
-// @version      1.45
+// @version      1.46
 // @description  decs supported: (Std-Frontier A), (Occ-Frontier B), (Smp-Frontier C), (Std-PreLodged D), (Occ-PreLodged E), (Smp-PreLodged F), (Clr-Frontier J), (Clr-PreLodged K), (Sup-SDP Y), (Sup-EIDR Z)
 // @author       You
 // @match        http*://*/customs-declare-exports*
@@ -297,7 +297,6 @@ function representativeType(){
         switch(getDeclaration()) {
             case 'A':
             case 'D':
-                break;
             case 'B':
             case 'C':
             case 'E':
