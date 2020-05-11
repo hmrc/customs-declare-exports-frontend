77d504e2d85e634dfbf91341517ef3119d8aa6c6
alsahid1993 <58818306+alsahid1993@users.noreply.github.com>
2020-05-11 11:02:14 +0100

    Revert "Merge pull request #888 from hmrc/CEDS-2383"
    This reverts commit 024642af3196d3b709bb3c26ddb343540e48c687, reversing
    changes made to e17cb7b3f85f6726d77060402cb37b152dd81241.

--- test/views/declaration/AdditionalInformationViewSpec.scala
+++ test/views/declaration/AdditionalInformationViewSpec.scala
@@ -17,8 +17,6 @@
 package views.declaration
 
 import base.Injector
-import controllers.declaration
-import controllers.declaration.routes
 import controllers.util.{Add, SaveAndContinue, SaveAndReturn}
 import forms.declaration.AdditionalInformation
 import helpers.views.declaration.CommonMessages
@@ -96,7 +94,7 @@ class AdditionalInformationViewSpec extends UnitViewSpec with ExportsTestData wi
         val backButton = createView().getElementById("back-link")
 
         backButton.text() mustBe messages(backCaption)
-        backButton.attr("href") mustBe routes.AdditionalInformationRequiredController.displayPage(Mode.Normal, itemId).url
+        backButton.attr("href") must endWith(s"/items/$itemId/commodity-measure")
       }
 
       "on the Simplified journey" in {
@@ -104,7 +102,7 @@ class AdditionalInformationViewSpec extends UnitViewSpec with ExportsTestData wi
         val backButton = createView(declarationType = DeclarationType.SIMPLIFIED).getElementById("back-link")
 
         backButton.text() mustBe messages(backCaption)
-        backButton.attr("href") mustBe routes.AdditionalInformationRequiredController.displayPage(Mode.Normal, itemId).url
+        backButton.attr("href") must endWith(s"/items/$itemId/package-information")
       }
     }
 
