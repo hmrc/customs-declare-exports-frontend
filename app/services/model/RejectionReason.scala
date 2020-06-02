20bb4b17a2823f2947ad91b0575b876391223f7c
Steve Sugden <46934286+SteveSugden@users.noreply.github.com>
2020-06-02 13:08:07 +0100

    CEDS-2467 - re-factor RejectionReason to include 'page error' message

--- app/services/model/RejectionReason.scala
+++ app/services/model/RejectionReason.scala
@@ -17,68 +17,41 @@
 package services.model
 
 import com.github.tototoshi.csv._
-import models.{ExportsDeclaration, Pointer}
+import config.AppConfig
+import javax.inject.{Inject, Singleton}
 import models.declaration.notifications.Notification
+import models.{ExportsDeclaration, Pointer}
 import play.api.Logger
 import play.api.i18n.Messages
 import play.api.libs.json.{Json, OFormat}
 
 import scala.io.Source
 
-case class RejectionReason(code: String, cdsDescription: String, exportsDescription: String, url: Option[String], pointer: Option[Pointer])
+case class RejectionReason(code: String, summaryErrorMessage: String, url: Option[String], pageErrorMessage: Option[String], pointer: Option[Pointer])
 
 object RejectionReason {
-
-  val allRejectionReasons: List[RejectionReason] = {
-    val reader =
-      CSVReader.open(Source.fromURL(getClass.getClassLoader.getResource("code-lists/errors-dms-rej-list.csv"), "UTF-8"))
-
-    val errors: List[List[String]] = reader.all()
-
-    errors.map(RejectionReason.apply)
-  }
-
-  def unknown(errorCode: String, pointer: Option[Pointer]) = RejectionReason(errorCode, "Unknown error", "Unknown error", None, pointer)
-
   implicit val format: OFormat[RejectionReason] = Json.format[RejectionReason]
   private val logger = Logger(this.getClass)
 
-  def apply(list: List[String]): RejectionReason = list match {
-    case code :: cdsDescription :: exportsDescription :: url :: Nil =>
-      RejectionReason(code, cdsDescription, applyExportsDescription(cdsDescription, exportsDescription), applyErrorUrl(url), None)
-    case error =>
-      logger.warn("Incorrect error: " + error)
-      throw new IllegalArgumentException("Error has incorrect structure")
+  def extractErrorDescription(useImprovedErrorMessage: Boolean, cdsDescription: String, exportsDescription: String): String = {
+    val useDescription = if (useImprovedErrorMessage) exportsDescription else cdsDescription
+    if (useDescription.isEmpty) cdsDescription else useDescription
   }
-
-  private def applyExportsDescription(cdsDescription: String, exportsDescription: String): String =
-    if (exportsDescription.isEmpty) cdsDescription else exportsDescription
 
   private def applyErrorUrl(url: String): Option[String] = if (url.isEmpty) None else Some(url)
 
-  def fromNotifications(notifications: Seq[Notification])(implicit messages: Messages): Seq[RejectionReason] = {
-    val rejectedNotification = notifications.find(_.isStatusRejected)
-
-    rejectedNotification.map { notification =>
-      notification.errors.map { error =>
-        val pointer = error.pointer.filter { p =>
-          val defined = messages.isDefinedAt(p.messageKey)
-          if (!defined) logger.warn("Missing error message key: " + p.messageKey)
-          defined
-        }
-
-        val url = error.url
-
-        val rejectedReason = allRejectionReasons
-          .find(_.code == error.validationCode)
-          .map(_.copy(pointer = pointer))
-          .getOrElse(unknown(error.validationCode, pointer))
-
-        if (url.isDefined) {
-          rejectedReason.copy(url = url)
-        } else rejectedReason
-      }
-    }.getOrElse(Seq.empty)
+  def apply(list: List[String], useImprovedErrorMessage: Boolean, pageErrorsMap: Map[String, String]): RejectionReason = list match {
+    case code :: cdsDescription :: exportsDescription :: url :: Nil =>
+      RejectionReason(
+        code,
+        extractErrorDescription(useImprovedErrorMessage, cdsDescription, exportsDescription),
+        applyErrorUrl(url),
+        pageErrorsMap.get(code),
+        None
+      )
+    case error =>
+      logger.warn("Incorrect error: " + error)
+      throw new IllegalArgumentException("Error has incorrect structure")
   }
 
   /**
@@ -122,4 +95,64 @@ object RejectionReason {
       }
     } else url
   }
+}
+
+@Singleton
+class RejectionReasons @Inject()(config: AppConfig) {
+
+  private val logger = Logger(this.getClass)
+
+  private val pageErrorsMap: Map[String, String] = {
+    def convertRow(list: List[String]): (String, String) = list match {
+      case code :: bratDescription :: exportsDescription :: Nil =>
+        code -> RejectionReason.extractErrorDescription(config.isUsingImprovedErrorMessages, bratDescription, exportsDescription)
+      case error =>
+        logger.warn("Incorrect error: " + error)
+        throw new IllegalArgumentException("PageErrors has incorrect structure")
+    }
+
+    val reader =
+      CSVReader.open(Source.fromURL(getClass.getClassLoader.getResource("code-lists/page-error-list.csv"), "UTF-8"))
+
+    val errors: List[List[String]] = reader.all()
+
+    errors.map(convertRow).toMap
+  }
+
+  val allRejectionReasons: List[RejectionReason] = {
+    val reader =
+      CSVReader.open(Source.fromURL(getClass.getClassLoader.getResource("code-lists/errors-dms-rej-list.csv"), "UTF-8"))
+
+    val errors: List[List[String]] = reader.all()
+
+    errors.map(errs => RejectionReason.apply(errs, config.isUsingImprovedErrorMessages, pageErrorsMap))
+  }
+
+  def unknown(errorCode: String, pointer: Option[Pointer]) = RejectionReason(errorCode, "Unknown error", None, None, pointer)
+
+  def fromNotifications(notifications: Seq[Notification])(implicit messages: Messages): Seq[RejectionReason] = {
+    val rejectedNotification = notifications.find(_.isStatusRejected)
+
+    rejectedNotification.map { notification =>
+      notification.errors.map { error =>
+        val pointer = error.pointer.filter { p =>
+          val defined = messages.isDefinedAt(p.messageKey)
+          if (!defined) logger.warn("Missing error message key: " + p.messageKey)
+          defined
+        }
+
+        val url = error.url
+
+        val rejectedReason = allRejectionReasons
+          .find(_.code == error.validationCode)
+          .map(_.copy(pointer = pointer))
+          .getOrElse(unknown(error.validationCode, pointer))
+
+        if (url.isDefined) {
+          rejectedReason.copy(url = url)
+        } else rejectedReason
+      }
+    }.getOrElse(Seq.empty)
+  }
+
 }
