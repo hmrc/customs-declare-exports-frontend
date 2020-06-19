0b642365e4b84fb71048e8243891ef809af70d31
Steve Sugden <46934286+SteveSugden@users.noreply.github.com>
2020-06-19 10:21:42 +0100

    BAU - action link item with correct govuk css class

---
+++ app/views/components/gds/ActionItemBuilder.scala
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.components.gds
+
+import play.api.i18n.Messages
+import uk.gov.hmrc.govukfrontend.views.viewmodels.content.Content
+import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.ActionItem
+
+object ActionItemBuilder {
+
+  def actionItem(href: String, content: Content, visuallyHiddenText: Option[String])(implicit messages: Messages) =
+    ActionItem(href = href, content = content, visuallyHiddenText = visuallyHiddenText, classes = "govuk-link--no-visited-state")
+}
