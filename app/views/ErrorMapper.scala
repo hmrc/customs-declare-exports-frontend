652b826a13456c8a97960209c17ff20c8cfe6545
Steve Sugden <46934286+SteveSugden@users.noreply.github.com>
2020-07-17 17:18:33 +0100

    CEDS-2569 - issue 2 - missing radio option error (part1)

---
+++ app/views/ErrorMapper.scala
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views
+
+import play.api.data.FormError
+
+object ErrorMapper {
+
+  def eoriOrAddressErrors(groupKey: String, errors: Seq[FormError]) =
+    errors.map(err => err.copy(key = if (err.key == groupKey) s"$groupKey.eori" else err.key))
+
+  def radioGroupErrors(groupKey: String, firstOption: String, errors: Seq[FormError]) =
+    errors.map(err => err.copy(key = if (err.key == groupKey) firstOption else err.key))
+}
