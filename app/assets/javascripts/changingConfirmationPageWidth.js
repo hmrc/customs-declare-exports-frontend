d75b5fe227288c5d1b76a371e264ddd9f12393f5
Maciej Rewera <22579925+MaciejRewera@users.noreply.github.com>
2020-09-11 14:49:38 +0100

    CEDS-2678 Make Confirmation page panel resizable
    The issue here was that for Supplementary declaration the green panel
    was too narrow causing text inside to overflow.
    
    The way this solution works is as follows:
    Solution depends on JavaScript to check current font size used by the
    browser. Based on this information a class describing column width for
    the main part of the page is set. The default width was changed to
    full-width to address non-JavaScript users.
    
    Solution was inspired by: https://alistapart.com/article/fontresizing/

---
+++ app/assets/javascripts/changingConfirmationPageWidth.js
@@ -0,0 +1,23 @@
+function init()  {
+    var iBase = TextResizeDetector.addEventListener(onFontResize, null);
+    adjustColumnWidth(iBase);
+}
+
+function onFontResize(e, args) {
+    var currentFontSize = args[0].iSize;
+    adjustColumnWidth(currentFontSize)
+}
+
+function adjustColumnWidth(fontSize) {
+    console.log("Changing page width for font of size " + fontSize);
+    var bodyContentClass = "govuk-grid-column-full";
+    if (fontSize < 30)
+        bodyContentClass = "govuk-grid-column-two-thirds";
+
+    document.getElementById("confirmation-content").setAttribute("class", bodyContentClass);
+}
+
+//id of element to check for and insert control
+TextResizeDetector.TARGET_ELEMENT_ID = 'main-content';
+//function to call once TextResizeDetector has init'd
+TextResizeDetector.USER_INIT_FUNC = init;
