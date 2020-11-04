baceb5a7b0cf306e103001597015aa4d1a90d1de
Maciej Rewera <22579925+MaciejRewera@users.noreply.github.com>
2020-11-04 10:07:17 +0000

    BAU Enhance MultipleItemsHelper
    Enhancement allows to pass field HMTL id in order to highlight given
    element on the page and provide error navigation.

--- app/controllers/util/MultipleItemsHelper.scala
+++ app/controllers/util/MultipleItemsHelper.scala
@@ -31,25 +31,26 @@ object MultipleItemsHelper {
     * @param form - multiple item form
     * @param cachedData - cached data which contains sequence of added items
     * @param limit - maximum limit of items
+    * @param fieldId - (optional) - field id of the HTML element to which the error should be attached
     * @tparam A - type of case class represents form
     * @return Either which can contain Form with errors or Sequence ready to insert to db
     */
-  def add[A](form: Form[A], cachedData: Seq[A], limit: Int, field: String = ""): Either[Form[A], Seq[A]] = form.value match {
-    case Some(document) => prepareData(form, document, cachedData, limit, field)
+  def add[A](form: Form[A], cachedData: Seq[A], limit: Int, fieldId: String = ""): Either[Form[A], Seq[A]] = form.value match {
+    case Some(document) => prepareData(form, document, cachedData, limit, fieldId)
     case _              => Left(form)
   }
 
-  private def prepareData[A](form: Form[A], document: A, cachedData: Seq[A], limit: Int, field: String): Either[Form[A], Seq[A]] =
-    (duplication(document, cachedData, field) ++ limitOfElems(limit, cachedData)) match {
+  private def prepareData[A](form: Form[A], document: A, cachedData: Seq[A], limit: Int, fieldId: String): Either[Form[A], Seq[A]] =
+    (duplication(document, cachedData, fieldId) ++ limitOfElems(limit, cachedData, fieldId)) match {
       case Seq()  => Right(addElement(document, cachedData))
       case errors => Left(form.copy(errors = errors))
     }
 
-  private def duplication[A](document: A, cachedData: Seq[A], field: String): Seq[FormError] =
-    if (cachedData.contains(document)) Seq(FormError(field, "supplementary.duplication")) else Seq.empty
+  private def duplication[A](document: A, cachedData: Seq[A], fieldId: String): Seq[FormError] =
+    if (cachedData.contains(document)) Seq(FormError(fieldId, "supplementary.duplication")) else Seq.empty
 
-  private def limitOfElems[A](limit: Int, cachedData: Seq[A]): Seq[FormError] =
-    if (cachedData.length >= limit) Seq(FormError("", "supplementary.limit")) else Seq.empty
+  private def limitOfElems[A](limit: Int, cachedData: Seq[A], fieldId: String): Seq[FormError] =
+    if (cachedData.length >= limit) Seq(FormError(fieldId, "supplementary.limit")) else Seq.empty
 
   private def addElement[A](document: A, cachedData: Seq[A]): Seq[A] = cachedData :+ document
 
@@ -62,20 +63,21 @@ object MultipleItemsHelper {
     * @param form - multiple item form
     * @param cachedData - cached data which contains sequence of added items
     * @param isMandatory - defines that screen is mandatory
+    * @param fieldId - (optional) - field id of the HTML element to which the error should be attached
     * @tparam A - type of case class represents form
     * @return Form with updated errors
     */
-  def continue[A](form: Form[A], cachedData: Seq[A], isMandatory: Boolean): Form[A] = {
-    val errors = checkInputs(form) ++ checkMandatory(isMandatory, cachedData)
+  def continue[A](form: Form[A], cachedData: Seq[A], isMandatory: Boolean, fieldId: String = ""): Form[A] = {
+    val errors = checkInputs(form, fieldId) ++ checkMandatory(isMandatory, cachedData, fieldId)
     form.copy(errors = errors)
   }
 
-  private def checkInputs[A](form: Form[A]): Seq[FormError] =
-    if (!isFormEmpty(form)) Seq(FormError("", "supplementary.continue.error"))
+  private def checkInputs[A](form: Form[A], fieldId: String): Seq[FormError] =
+    if (!isFormEmpty(form)) Seq(FormError(fieldId, "supplementary.continue.error"))
     else Seq.empty
 
-  private def checkMandatory[A](isMandatory: Boolean, cachedData: Seq[A]): Seq[FormError] =
-    if (isMandatory && cachedData.isEmpty) Seq(FormError("", "supplementary.continue.mandatory"))
+  private def checkMandatory[A](isMandatory: Boolean, cachedData: Seq[A], fieldId: String): Seq[FormError] =
+    if (isMandatory && cachedData.isEmpty) Seq(FormError(fieldId, "supplementary.continue.mandatory"))
     else Seq.empty
 
   /**
@@ -85,19 +87,20 @@ object MultipleItemsHelper {
     * @param cachedData - cached data which contains sequence of added items
     * @param isMandatory - defines that screen is mandatory
     * @param limit - limit of elements allowed for page
+    * @param fieldId - (optional) - field id of the HTML element to which the error should be attached
     * @tparam A - type of case class represents form
     * @return Form with updated errors or Sequence ready to insert to db
     */
-  def saveAndContinue[A](form: Form[A], cachedData: Seq[A], isMandatory: Boolean, limit: Int, field: String = ""): Either[Form[A], Seq[A]] =
-    if (!isFormEmpty(form)) add(form, cachedData, limit, field)
+  def saveAndContinue[A](form: Form[A], cachedData: Seq[A], isMandatory: Boolean, limit: Int, fieldId: String = ""): Either[Form[A], Seq[A]] =
+    if (!isFormEmpty(form)) add(form, cachedData, limit, fieldId)
     else {
-      val mandatoryFieldsError = checkMandatory(isMandatory, cachedData)
+      val mandatoryFieldsError = checkMandatory(isMandatory, cachedData, fieldId)
       if (mandatoryFieldsError.nonEmpty) Left(form.copy(errors = mandatoryFieldsError))
       else Right(cachedData)
     }
 
   private def isFormEmpty[A](form: Form[A]): Boolean =
-    retrieveData(form).filter { case (_, value) => value.nonEmpty }.isEmpty
+    !retrieveData(form).exists { case (_, value) => value.nonEmpty }
 
   private def retrieveData[A](form: Form[A]): Map[String, String] =
     form.data.filter { case (name, _) => name != "csrfToken" }
