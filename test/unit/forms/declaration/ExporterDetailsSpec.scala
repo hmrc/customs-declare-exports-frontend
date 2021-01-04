b9ca393f3afdc36842c3dc087a6d8ab55362d1ad
Maciej Rewera <22579925+MaciejRewera@users.noreply.github.com>
2021-01-04 13:22:20 +0000

    BAU Update copyright header in files

--- test/unit/forms/declaration/ExporterDetailsSpec.scala
+++ test/unit/forms/declaration/ExporterDetailsSpec.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 HM Revenue & Customs
+ * Copyright 2021 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,6 +22,7 @@ import forms.declaration.exporter.ExporterDetails
 import models.DeclarationType._
 import models.ExportsDeclaration
 import org.scalatest.{MustMatchers, WordSpec}
+import play.api.data.Form
 import unit.base.JourneyTypeTestRunner
 
 class ExporterDetailsSpec extends WordSpec with MustMatchers with LightFormMatchers with JourneyTypeTestRunner {
