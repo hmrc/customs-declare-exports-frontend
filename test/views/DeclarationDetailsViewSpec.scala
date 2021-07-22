db516730ada990db7f61f2403efbf8733d311891
Lucio Biondi <lucio.biondi@digital.hmrc.gov.uk>
2021-07-22 14:03:36 +0100

    CEDS-3261 Add section B's links

--- test/views/DeclarationDetailsViewSpec.scala
+++ test/views/DeclarationDetailsViewSpec.scala
@@ -24,9 +24,10 @@ import base.{Injector, OverridableInjector}
 import config.featureFlags._
 import controllers.routes
 import models.declaration.notifications.Notification
-import models.declaration.submissions.Submission
-import models.declaration.submissions.SubmissionStatus.{ACCEPTED, RECEIVED, REJECTED, UNKNOWN}
+import models.declaration.submissions.{Submission, SubmissionStatus}
+import models.declaration.submissions.SubmissionStatus._
 import org.mockito.Mockito.when
+import org.scalatest.Assertion
 import play.api.inject.bind
 import views.declaration.spec.UnitViewSpec
 import views.helpers.{StatusOfSubmission, ViewDates}
@@ -38,22 +39,22 @@ class DeclarationDetailsViewSpec extends UnitViewSpec with Injector {
 
   private val now = ZonedDateTime.now
 
-  private val notifications = List(
-    Notification("id", "mrn", now, RECEIVED, Seq.empty),
-    Notification("id", "mrn", now, UNKNOWN, Seq.empty),
-    Notification("id", "mrn", now, ACCEPTED, Seq.empty),
-    Notification("id", "mrn", now, REJECTED, Seq.empty)
-  )
+  private val acceptedNotification = Notification("id", "mrn", now, ACCEPTED, Seq.empty)
+  private val clearedNotification = Notification("id", "mrn", now, CLEARED, Seq.empty)
+  private val rejectedNotification = Notification("id", "mrn", now, REJECTED, Seq.empty)
+  private val unknownNotification = Notification("id", "mrn", now, UNKNOWN, Seq.empty)
+
+  private val notifications = List(unknownNotification, clearedNotification, acceptedNotification, rejectedNotification)
 
   "Declaration details page" should {
 
     val secureMessagingConfig = mock[SecureMessagingConfig]
     val injector = new OverridableInjector(bind[SecureMessagingConfig].toInstance(secureMessagingConfig))
+    val page = injector.instanceOf[declaration_details]
 
     "contain the navigation banner" when {
-      "the Secure Messaging flag is set to 'true'" in {
+      "the Secure Messaging feature flag is enabled" in {
         when(secureMessagingConfig.isSecureMessagingEnabled).thenReturn(true)
-        val page = injector.instanceOf[declaration_details]
         val view = page(submission, notifications)(request, messages)
 
         val banner = view.getElementById("navigation-banner")
@@ -67,11 +68,61 @@ class DeclarationDetailsViewSpec extends UnitViewSpec with Injector {
     }
 
     "not contain the navigation banner" when {
-      "the Secure Messaging flag is set to 'false'" in {
+      "the Secure Messaging feature flag is disabled" in {
         when(secureMessagingConfig.isSecureMessagingEnabled).thenReturn(false)
-        val page = injector.instanceOf[declaration_details]
         val view = page(submission, notifications)(request, messages)
         Option(view.getElementById("navigation-banner")) mustBe None
+      }
+    }
+  }
+
+  "Declaration details page" when {
+
+    val eadConfig = mock[EadConfig]
+    val injector = new OverridableInjector(bind[EadConfig].toInstance(eadConfig))
+    val page = injector.instanceOf[declaration_details]
+
+    "the EAD feature flag is enabled" should {
+      when(eadConfig.isEadEnabled).thenReturn(true)
+
+      "contain the PDF-for-EAD link for any accepted notification's status" in {
+        SubmissionStatus.values
+          .filter(eadAcceptableStatuses.contains)
+          .foreach(status => verifyPdfForEadLink(unknownNotification.copy(status = status)))
+      }
+
+      "not contain the PDF-for-EAD link" when {
+
+        "the notification's status is not an accepted status" in {
+          SubmissionStatus.values
+            .filterNot(eadAcceptableStatuses.contains)
+            .foreach { status =>
+              val view = page(submission, List(unknownNotification.copy(status = status)))(request, messages)
+              Option(view.getElementById("generate-ead")) mustBe None
+            }
+        }
+
+        "there is no mrn" in {
+          val view = page(submission.copy(mrn = None), notifications)(request, messages)
+          Option(view.getElementById("generate-ead")) mustBe None
+        }
+      }
+
+      def verifyPdfForEadLink(notification: Notification): Assertion = {
+        val view = page(submission, List(notification))(request, messages)
+
+        val declarationLink = view.getElementById("generate-ead")
+        declarationLink must containMessage("submissions.generateEAD")
+        declarationLink must haveHref(controllers.pdf.routes.EADController.generatePdf(submission.mrn.get))
+      }
+    }
+
+    "the EAD feature flag is disabled" should {
+      "not contain the PDF-for-EAD link" in {
+        when(eadConfig.isEadEnabled).thenReturn(false)
+        val page = injector.instanceOf[declaration_details]
+        val view = page(submission, notifications)(request, messages)
+        Option(view.getElementById("generate-ead")) mustBe None
       }
     }
   }
@@ -116,6 +167,18 @@ class DeclarationDetailsViewSpec extends UnitViewSpec with Injector {
       val lrn = view.getElementsByClass("submission-lrn").get(0)
       lrn.getElementsByClass("govuk-summary-list__key").text mustBe messages("submissions.declarationDetails.lrn")
       lrn.getElementsByClass("govuk-summary-list__value").text mustBe submission.lrn
+    }
+
+    "always contain the view-declaration link, regardless of the notification's status" when {
+      def verifyDeclarationLink(notification: Notification): Assertion = {
+        val view = page(submission, List(notification))(request, messages)
+
+        val declarationLink = view.getElementById("view-declaration")
+        declarationLink must containMessage("submissions.viewDeclaration")
+        declarationLink must haveHref(routes.SubmissionsController.viewDeclaration(submission.uuid))
+      }
+
+      SubmissionStatus.values foreach (status => verifyDeclarationLink(unknownNotification.copy(status = status)))
     }
 
     "display the Declaration Timeline when there is at least one notification for the declaration" in {
