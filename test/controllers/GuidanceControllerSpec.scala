e65c5bbcdf7afd8588a01ef5af3134bba0fa65d3
Tony Merryfield <tony.merryfield@oddbunch.co.uk>
2021-08-20 10:25:06 +0100

    CEDS-3378 - Static content version of the error explanation page (#1277)
    * Static content version of the error explanation page
    
    * Fixing a scala formatting issue
    
    * Tidy up and making more generic
    
    * Link change
    
    * CEDS-3378 Basic unit tests for the 'Error Explanation' page, controller and template
    
    Co-authored-by: Lucio Biondi <lucio.biondi@digital.hmrc.gov.uk>

---
+++ test/controllers/GuidanceControllerSpec.scala
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import base.ControllerWithoutFormSpec
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.when
+import play.api.test.Helpers._
+import play.twirl.api.HtmlFormat
+import views.html.guidance.error_explanation
+
+class GuidanceControllerSpec extends ControllerWithoutFormSpec {
+
+  val errorExplanationPage = mock[error_explanation]
+  val controller = new GuidanceController(stubMessagesControllerComponents(), errorExplanationPage)
+
+  "GuidanceController" should {
+
+    "return 200 (OK)" when {
+
+      "the errorExplanation method is invoked" in {
+        when(errorExplanationPage.apply()(any(), any())).thenReturn(HtmlFormat.empty)
+
+        val result = controller.errorExplanation()(getRequest())
+        status(result) must be(OK)
+      }
+    }
+  }
+}
