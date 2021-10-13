29083150f3e0214b8765f4b30770f4c21ceaed93
Tony Merryfield (Razer Blade 15) <tony.merryfield@oddbunch.co.uk>
2021-10-13 13:01:46 +0100

    ceds-3428 - Adding EORI number to title

--- test/views/declaration/DeclarantDetailsViewSpec.scala
+++ test/views/declaration/DeclarantDetailsViewSpec.scala
@@ -45,7 +45,6 @@ class DeclarantDetailsViewSpec extends UnitViewSpec with ExportsTestData with Co
   "Declarant Details View on empty page" should {
 
     "have correct message keys" in {
-      messages must haveTranslationFor("declaration.declarant.title")
       messages must haveTranslationFor("declaration.eori.error.format")
       messages must haveTranslationFor("declaration.eori.empty")
     }
@@ -57,7 +56,7 @@ class DeclarantDetailsViewSpec extends UnitViewSpec with ExportsTestData with Co
       "display page title" in {
 
         createView(form())
-          .getElementsByClass(Styles.gdsPageLegend) must containMessageForElements("declaration.declarant.titleQuestion", request.eori)
+          .getElementsByClass(Styles.gdsPageLegend) must containMessageForElements("declaration.declarant.title", request.eori)
       }
 
       "display radio button with Yes option" in {
