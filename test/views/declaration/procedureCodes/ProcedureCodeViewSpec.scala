2dfd3b557dd63f8b48357b3c357cde028bcee137
darrylmurphy <darrylmurphy@users.noreply.github.com>
2021-10-14 13:45:13 +0100

    CEDS-3525: Revert changes made in 3470 to feed msg keys for select accessibility

--- test/views/declaration/procedureCodes/ProcedureCodeViewSpec.scala
+++ test/views/declaration/procedureCodes/ProcedureCodeViewSpec.scala
@@ -42,6 +42,7 @@ class ProcedureCodeViewSpec extends UnitViewSpec with ExportsTestData with Injec
     "have proper messages for labels" in {
       messages must haveTranslationFor("declaration.procedureCodes.title")
       messages must haveTranslationFor("declaration.procedureCodes.hint")
+      messages must haveTranslationFor("declaration.procedureCodes.empty")
       messages must haveTranslationFor("declaration.procedureCodes.error.empty")
       messages must haveTranslationFor("declaration.procedureCodes.error.invalid")
 
