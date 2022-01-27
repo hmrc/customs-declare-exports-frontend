3ccaee3e5c56fe407f83790c5a35589a3621d2ab
darrylmurphy <darrylmurphy@users.noreply.github.com>
2022-01-27 12:10:10 +0000

    CEDS-3709: add check to nav for authCodes list

--- test/controllers/declaration/IsLicenseRequiredControllerSpec.scala
+++ test/controllers/declaration/IsLicenseRequiredControllerSpec.scala
@@ -19,6 +19,7 @@ package controllers.declaration
 import base.ControllerSpec
 import forms.common.YesNoAnswer
 import forms.declaration.CommodityDetails
+import forms.declaration.declarationHolder.AuthorizationTypeCodes
 import models.{DeclarationType, Mode}
 import org.mockito.ArgumentCaptor
 import org.mockito.ArgumentMatchers.any
@@ -114,14 +115,32 @@ class IsLicenseRequiredControllerSpec extends ControllerSpec with OptionValues {
           thePageNavigatedTo mustBe routes.AdditionalDocumentAddController.displayPage(Mode.Normal, itemId)
         }
 
-        "user submits valid No answer" in {
-          withNewCaching(declaration)
+        "user submits valid No answer" when {
+          "NO authorisation from List" in {
+            withNewCaching(declaration)
 
-          val requestBody = Seq("yesNo" -> "No")
-          val result = controller.submitForm(Mode.Normal, itemId)(postRequestAsFormUrlEncoded(requestBody: _*))
+            val requestBody = Seq("yesNo" -> "No")
+            val result = controller.submitForm(Mode.Normal, itemId)(postRequestAsFormUrlEncoded(requestBody: _*))
 
-          await(result) mustBe aRedirectToTheNextPage
-          thePageNavigatedTo mustBe routes.AdditionalDocumentsRequiredController.displayPage(Mode.Normal, itemId)
+            await(result) mustBe aRedirectToTheNextPage
+            thePageNavigatedTo mustBe routes.AdditionalDocumentsRequiredController.displayPage(Mode.Normal, itemId)
+          }
+
+          "authorisation from List" in {
+
+            val declaration = aDeclaration(
+              withItem(anItem(withItemId(itemId), withCommodityDetails(commodityDetails))),
+              withDeclarationHolders(authorisationTypeCode = Some(AuthorizationTypeCodes.codesRequiringDocumentation.head))
+            )
+
+            withNewCaching(declaration)
+
+            val requestBody = Seq("yesNo" -> "No")
+            val result = controller.submitForm(Mode.Normal, itemId)(postRequestAsFormUrlEncoded(requestBody: _*))
+
+            await(result) mustBe aRedirectToTheNextPage
+            thePageNavigatedTo mustBe routes.AdditionalDocumentsController.displayPage(Mode.Normal, itemId)
+          }
         }
       }
     }
