78e6bd11227e3a83097549e88c8382b8075bfc89
darrylmurphy <darrylmurphy@users.noreply.github.com>
2022-01-28 15:40:24 +0000

    CEDS-3709: redirecting additional-info to is-license-required

--- app/controllers/declaration/IsLicenseRequiredController.scala
+++ app/controllers/declaration/IsLicenseRequiredController.scala
@@ -27,7 +27,7 @@ import models.requests.JourneyRequest
 import models.{ExportsDeclaration, Mode}
 import play.api.data.Form
 import play.api.i18n.I18nSupport
-import play.api.mvc._
+import play.api.mvc.{request, _}
 import services.cache.ExportsCacheService
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
 import views.html.declaration.is_license_required
