7c65edf46bcb32c65b599f5b17acad80fe3f6f26
darrylmurphy <darrylmurphy@users.noreply.github.com>
2022-03-02 14:57:37 +0000

    CEDS-3788: start on ff for 999L waiver with action

---
+++ app/controllers/actions/FeatureFlagAction.scala
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.actions
+
+import com.google.inject.Inject
+import config.featureFlags.FeatureSwitchConfig
+import controllers.routes.RootController
+import features.Feature
+import models.requests.JourneyRequest
+import play.api.Logging
+import play.api.mvc.{ActionRefiner, Result, Results}
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.play.http.HeaderCarrierConverter
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class FeatureFlagAction @Inject()(featureSwitchConfig: FeatureSwitchConfig)(implicit val exc: ExecutionContext)
+    extends ActionRefiner[JourneyRequest, JourneyRequest] with Logging {
+
+  type RefineResult[A] = Future[Either[Result, JourneyRequest[A]]]
+
+  override protected def executionContext: ExecutionContext = exc
+
+  override def refine[A](request: JourneyRequest[A]): RefineResult[A] =
+    Future.successful(Right(request))
+
+  def apply(feature: Feature.Value): ActionRefiner[JourneyRequest, JourneyRequest] =
+    new ActionRefiner[JourneyRequest, JourneyRequest] {
+      override protected def executionContext: ExecutionContext = exc
+
+      override protected def refine[A](request: JourneyRequest[A]): RefineResult[A] = refineOnFeatureFlag(request, feature)
+    }
+
+  private def refineOnFeatureFlag[A](request: JourneyRequest[A], feature: Feature.Value): RefineResult[A] = {
+    implicit val hc: HeaderCarrier = HeaderCarrierConverter.fromRequestAndSession(request, request.session)
+
+    Future.successful {
+      if (featureSwitchConfig.isFeatureOn(feature)) Right(request)
+      else {
+        logger.warn(s"Redirection to start for eori ${request.user.eori}, as feature flag $feature is disabled")
+        Left(Results.Redirect(RootController.displayPage()))
+      }
+    }
+
+  }
+}
---
+++ test/controllers/actions/FeatureFlagActionSpec.scala
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.actions
+
+import base.{RequestBuilder, UnitWithMocksSpec}
+import com.typesafe.config.ConfigFactory
+import config.featureFlags.FeatureSwitchConfig
+import models.requests.JourneyRequest
+import models.{IdentityData, SignedInUser}
+import org.mockito.ArgumentCaptor
+import org.mockito.ArgumentMatchers._
+import org.mockito.BDDMockito._
+import org.mockito.Mockito._
+import org.scalatest.BeforeAndAfterEach
+import play.api.Configuration
+import play.api.mvc.{AnyContentAsEmpty, Result, Results}
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import services.cache.{ExportsCacheService, ExportsDeclarationBuilder}
+import uk.gov.hmrc.auth.core.Enrolments
+import uk.gov.hmrc.http.HeaderCarrier
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+class FeatureFlagActionSpec extends UnitWithMocksSpec with BeforeAndAfterEach with ExportsDeclarationBuilder with RequestBuilder {
+
+  private val cache = mock[ExportsCacheService]
+  private val block = mock[JourneyRequest[_] => Future[Result]]
+  private val user = SignedInUser("eori", Enrolments(Set.empty), IdentityData())
+  private val declaration = aDeclaration()
+  private val refiner = new FeatureFlagAction(
+    new FeatureSwitchConfig(Configuration(ConfigFactory.parseString("microservice.services.features.default=disabled")))
+  )
+
+  private def request(declarationId: Option[String]): FakeRequest[AnyContentAsEmpty.type] = declarationId match {
+    case Some(id) =>
+      FakeRequest().withSession("declarationId" -> id)
+    case None => FakeRequest()
+  }
+
+  override def afterEach(): Unit = {
+    reset(cache, block)
+    super.afterEach()
+  }
+
+  "refine" should {
+
+    "permit request" when {
+      "feature flag enabled" in {
+        given(block.apply(any())).willReturn(Future.successful(Results.Ok))
+        given(cache.get(refEq("id"))(any[HeaderCarrier])).willReturn(Future.successful(Some(declaration)))
+
+        await(refiner.invokeBlock(???, block)) mustBe Results.Ok
+
+        val result = theRequestBuilt
+        result.cacheModel mustBe declaration
+      }
+
+      def theRequestBuilt: JourneyRequest[_] = {
+        val captor = ArgumentCaptor.forClass(classOf[JourneyRequest[_]])
+        verify(block).apply(captor.capture())
+        captor.getValue
+      }
+    }
+
+    "block request" when {
+
+      "feature flag disabled" in {
+        given(cache.get(refEq("id"))(any[HeaderCarrier])).willReturn(Future.successful(None))
+
+        await(refiner.invokeBlock(???, block)) mustBe Results.Redirect(controllers.routes.RootController.displayPage())
+      }
+
+    }
+  }
+}
