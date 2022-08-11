3af3fac778ef5ddc22c90a06593c394cc2208cc5
Lucio Biondi <lucio.biondi@digital.hmrc.gov.uk>
2022-08-11 08:34:20 +0100

    CEDS-4054 Add to the Summary page a link to /type when a DRAFT declaration has 'parentDeclarationId' defined

--- test/views/declaration/summary/sections/ReferencesSectionViewSpec.scala
+++ test/views/declaration/summary/sections/ReferencesSectionViewSpec.scala
@@ -20,8 +20,10 @@ import base.Injector
 import controllers.declaration.routes
 import forms.common.YesNoAnswer.YesNoAnswers
 import forms.declaration.additionaldeclarationtype.AdditionalDeclarationType
-import models.DeclarationType.{CLEARANCE, OCCASIONAL, SIMPLIFIED, STANDARD, SUPPLEMENTARY}
-import models.{DeclarationType, Mode}
+import models.DeclarationStatus.DRAFT
+import models.DeclarationType
+import models.DeclarationType._
+import models.Mode.Change
 import services.cache.ExportsTestHelper
 import views.declaration.spec.UnitViewSpec
 import views.html.declaration.summary.sections.references_section
@@ -38,48 +40,62 @@ class ReferencesSectionViewSpec extends UnitViewSpec with ExportsTestHelper with
 
   val section = instanceOf[references_section]
 
-  val view = section(Mode.Change, data)(messages)
-  val viewNoAnswers = section(Mode.Change, aDeclaration(withType(DeclarationType.STANDARD)))(messages)
+  val view = section(Change, data)(messages)
+  val viewNoAnswers = section(Change, aDeclaration(withType(DeclarationType.STANDARD)))(messages)
 
   "References section" should {
 
-    "have capitalized declaration type with change button" in {
+    "have, inside an Inset Text, a link to /type" when {
+      "a declaration has DRAFT status and 'parentDeclarationId' is defined" in {
+        val declaration = data.copy(status = DRAFT, parentDeclarationId = Some("some id"))
+        val insetText = section(Change, declaration)(messages).getElementsByClass("govuk-inset-text")
+        insetText.size mustBe 1
 
+        val link = insetText.first.getElementsByClass("govuk-link").first
+        link.text mustBe messages("declaration.summary.goto.additional.type")
+        link must haveHref(routes.AdditionalDeclarationTypeController.displayPage(Change))
+      }
+    }
+
+    "not have any Inset Text" when {
+      "a declaration has 'parentDeclarationId' undefined" in {
+        view.getElementsByClass("govuk-inset-text").size mustBe 0
+      }
+    }
+
+    "have capitalized declaration type with change button" in {
       val row = view.getElementsByClass("declarationType-row")
       row must haveSummaryKey(messages("declaration.summary.references.type"))
       row must haveSummaryValue("Standard declaration")
 
       row must haveSummaryActionsTexts("site.change", "declaration.summary.references.type.change")
 
-      row must haveSummaryActionsHref(routes.DeclarationChoiceController.displayPage(Mode.Change))
+      row must haveSummaryActionsHref(routes.DeclarationChoiceController.displayPage(Change))
     }
 
     "have additional declaration type with change button" in {
-
       val row = view.getElementsByClass("additionalType-row")
       row must haveSummaryKey(messages("declaration.summary.references.additionalType"))
       row must haveSummaryValue(messages("declaration.summary.references.additionalType.A"))
 
       row must haveSummaryActionsTexts("site.change", "declaration.summary.references.additionalType.change")
 
-      row must haveSummaryActionsHref(routes.AdditionalDeclarationTypeController.displayPage(Mode.Change))
+      row must haveSummaryActionsHref(routes.AdditionalDeclarationTypeController.displayPage(Change))
     }
 
     "have DUCR with change button" in {
-
       val row = view.getElementsByClass("ducr-row")
       row must haveSummaryKey(messages("declaration.summary.references.ducr"))
       row must haveSummaryValue("DUCR")
 
       row must haveSummaryActionsTexts("site.change", "declaration.summary.references.ducr.change")
 
-      row must haveSummaryActionsHref(routes.ConsignmentReferencesController.displayPage(Mode.Change))
+      row must haveSummaryActionsHref(routes.ConsignmentReferencesController.displayPage(Change))
     }
 
     onJourney(STANDARD, SIMPLIFIED, OCCASIONAL, CLEARANCE) { implicit request =>
       "have LRN with change button" in {
-
-        val view = section(Mode.Change, data.copy(`type` = request.declarationType))(messages)
+        val view = section(Change, data.copy(`type` = request.declarationType))(messages)
 
         val row = view.getElementsByClass("lrn-row")
         row must haveSummaryKey(messages("declaration.summary.references.lrn"))
@@ -87,14 +103,13 @@ class ReferencesSectionViewSpec extends UnitViewSpec with ExportsTestHelper with
 
         row must haveSummaryActionsTexts("site.change", "declaration.summary.references.lrn.change")
 
-        row must haveSummaryActionsHref(routes.ConsignmentReferencesController.displayPage(Mode.Change))
+        row must haveSummaryActionsHref(routes.ConsignmentReferencesController.displayPage(Change))
       }
     }
 
     onJourney(SUPPLEMENTARY) { implicit request =>
       "have LRN with change button" in {
-
-        val view = section(Mode.Change, data.copy(`type` = request.declarationType))(messages)
+        val view = section(Change, data.copy(`type` = request.declarationType))(messages)
 
         val row = view.getElementsByClass("lrn-row")
         row must haveSummaryKey(messages("declaration.summary.references.supplementary.lrn"))
@@ -102,54 +117,48 @@ class ReferencesSectionViewSpec extends UnitViewSpec with ExportsTestHelper with
 
         row must haveSummaryActionsTexts("site.change", "declaration.summary.references.lrn.change")
 
-        row must haveSummaryActionsHref(routes.ConsignmentReferencesController.displayPage(Mode.Change))
+        row must haveSummaryActionsHref(routes.ConsignmentReferencesController.displayPage(Change))
       }
     }
 
     "have 'Link to a MUCR' with change button" in {
-
       val row = view.getElementsByClass("linkDucrToMucr-row")
       row must haveSummaryKey(messages("declaration.summary.references.linkDucrToMucr"))
       row must haveSummaryValue(YesNoAnswers.yes)
 
       row must haveSummaryActionsTexts("site.change", "declaration.summary.references.linkDucrToMucr.change")
 
-      row must haveSummaryActionsHref(routes.LinkDucrToMucrController.displayPage(Mode.Change))
+      row must haveSummaryActionsHref(routes.LinkDucrToMucrController.displayPage(Change))
     }
 
     "have MUCR with change button" in {
-
       val row = view.getElementsByClass("mucr-row")
       row must haveSummaryKey(messages("declaration.summary.references.mucr"))
       row must haveSummaryValue(MUCR.mucr)
 
       row must haveSummaryActionsTexts("site.change", "declaration.summary.references.mucr.change")
 
-      row must haveSummaryActionsHref(routes.MucrController.displayPage(Mode.Change))
+      row must haveSummaryActionsHref(routes.MucrController.displayPage(Change))
     }
   }
 
   "References section with no answers" should {
 
     "have declaration type" in {
-
       val row = viewNoAnswers.getElementsByClass("declarationType-row")
       row must haveSummaryKey(messages("declaration.summary.references.type"))
-      row must haveSummaryActionsHref(routes.DeclarationChoiceController.displayPage(Mode.Change))
+      row must haveSummaryActionsHref(routes.DeclarationChoiceController.displayPage(Change))
     }
 
     "not have additional declaration type" in {
-
       viewNoAnswers.getElementsByClass("additionalType-row") mustBe empty
     }
 
     "not have have ducr" in {
-
       viewNoAnswers.getElementsByClass("ducr-row") mustBe empty
     }
 
     "not have have lrn" in {
-
       viewNoAnswers.getElementsByClass("lrn-row") mustBe empty
     }
   }
