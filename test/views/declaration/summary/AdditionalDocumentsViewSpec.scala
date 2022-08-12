eac0f6bdc8302cf9b5cbfb8e7b610f0ee23170de
Lucio Biondi <lucio.biondi@digital.hmrc.gov.uk>
2022-08-12 11:33:34 +0100

    CEDS-3939 Licences answer not showing on Summary page when AdditionalDocuments is undefined

--- test/views/declaration/summary/AdditionalDocumentsViewSpec.scala
+++ test/views/declaration/summary/AdditionalDocumentsViewSpec.scala
@@ -19,198 +19,182 @@ package views.declaration.summary
 import base.Injector
 import com.typesafe.config.ConfigFactory
 import controllers.declaration.routes
-import forms.common.YesNoAnswer.Yes
+import forms.common.YesNoAnswer.{Yes, YesNoAnswers}
 import forms.declaration.additionaldocuments.AdditionalDocument
-import models.declaration.AdditionalDocuments
-import models.{DeclarationType, Mode}
+import models.DeclarationType._
+import models.Mode.Normal
+import models.declaration.{AdditionalDocuments, ExportItem}
+import org.jsoup.select.Elements
+import org.scalatest.Assertion
 import play.api.Configuration
+import play.twirl.api.HtmlFormat.Appendable
 import services.cache.ExportsTestHelper
 import views.declaration.spec.UnitViewSpec
 import views.html.declaration.summary.additional_documents
 
 class AdditionalDocumentsViewSpec extends UnitViewSpec with ExportsTestHelper {
 
-  private val item = anItem(withItemId("itemId"), withSequenceId(1))
-  private val itemWithLicenceReq = anItem(withItemId("itemId"), withSequenceId(1)).copy(isLicenceRequired = Some(true))
-  private val itemWithNoLicenceReq = anItem(withItemId("itemId"), withSequenceId(1)).copy(isLicenceRequired = Some(false))
-
   private val documents = Seq(
     AdditionalDocument(Some("typ1"), Some("identifier1"), None, None, None, None, None),
     AdditionalDocument(Some("typ2"), Some("identifier2"), None, None, None, None, None)
   )
 
-  "Supporting documents view" when {
+  private def itemWithLicenceReq(
+    licenceRequired: String,
+    maybeAdditionalDocuments: Option[AdditionalDocuments] = Some(AdditionalDocuments(Yes, documents))
+  ): ExportItem = {
+    val isLicenceRequired = Some(licenceRequired == YesNoAnswers.yes)
+    val additionalDocuments = maybeAdditionalDocuments.fold(withoutAdditionalInformation)(withAdditionalDocuments)
+    anItem(withItemId("itemId"), withSequenceId(1), additionalDocuments).copy(isLicenceRequired = isLicenceRequired)
+  }
 
+  private def itemWithNoLicenceReq(maybeAdditionalDocuments: Option[AdditionalDocuments] = Some(AdditionalDocuments(Yes, documents))): ExportItem = {
+    val additionalDocuments = maybeAdditionalDocuments.fold(withoutAdditionalInformation)(withAdditionalDocuments)
+    anItem(withItemId("itemId"), withSequenceId(1), additionalDocuments)
+  }
+
+  "AdditionalDocuments view" when {
+
+    val is999LKey = "microservice.services.features.waiver999L"
+    val config = ConfigFactory.parseString(s"$is999LKey=enabled")
     val injector = new Injector {
-      override val configuration: Configuration = Configuration(ConfigFactory.parseString("microservice.services.features.waiver999L=enabled"))
+      override val configuration: Configuration = Configuration(config)
     }
     val additionalDocumentsSection = injector.instanceOf[additional_documents]
 
-    "without documents or defined licence answer" should {
+    "with additionalDocuments defined but without actual documents or defined licence answer" should {
+      "display title and 'No Additional documents' row only" in {
+        val view = additionalDocumentsSection(Normal, itemWithNoLicenceReq(Some(AdditionalDocuments(None, Seq.empty))))(messages)
+        verifyHeader(view)
 
-      "display title and additional docs row only" in {
+        view.getElementsByClass("licences-1-row") mustBe empty
+        view.getElementsByTag("table") mustBe empty
 
-        val view = additionalDocumentsSection(Mode.Normal, item, AdditionalDocuments(None, Seq.empty), DeclarationType.STANDARD)(messages)
-        val section = view.getElementById("additional-docs-section-item-1")
-        val docsRow = view.getElementsByClass("additional-documents-1-row")
-
-        section.child(0) must containMessage("declaration.summary.items.item.additionalDocuments")
-
-        Option(view.getElementsByClass("licences-1-row").first()) must be(None)
-
-        docsRow must haveSummaryKey(messages("declaration.summary.items.item.additionalDocuments"))
-        docsRow must haveSummaryValue("No")
-        docsRow must haveSummaryActionsTexts("site.change", "declaration.summary.items.item.additionalDocuments.changeAll", "1")
-        docsRow must haveSummaryActionsHref(routes.AdditionalDocumentsController.displayPage(Mode.Normal, "itemId"))
+        verifyNoAdditionalDocuments(view, config.getString(is999LKey))
       }
-
     }
 
-    "with documents and licence answer" should {
+    List(YesNoAnswers.yes, YesNoAnswers.no).foreach { answer =>
+      s"with licence answer equal to '$answer' and" when {
 
-      onJourney(DeclarationType.STANDARD, DeclarationType.SUPPLEMENTARY, DeclarationType.SIMPLIFIED, DeclarationType.OCCASIONAL)(
-        aDeclaration(withItem(itemWithLicenceReq))
-      ) { implicit request =>
-        "licence required" should {
-          "display licences row" in {
-            val view =
-              additionalDocumentsSection(Mode.Normal, itemWithLicenceReq, AdditionalDocuments(Yes, documents), request.declarationType)(messages)
-            val licencesRow = view.getElementsByClass("licences-1-row")
-
-            licencesRow must haveSummaryKey(messages("declaration.summary.items.item.licences"))
-
-          }
-
-          "show h3 header" in {
-
-            val view =
-              additionalDocumentsSection(Mode.Normal, itemWithLicenceReq, AdditionalDocuments(Yes, documents), request.declarationType)(messages)
-
-            view.getElementsByTag("h3") must containMessageForElements("declaration.summary.items.item.additionalDocuments")
-
+        onJourney(STANDARD, SUPPLEMENTARY, SIMPLIFIED, OCCASIONAL)(aDeclaration(withItem(itemWithLicenceReq(answer)))) { implicit request =>
+          "with Additional Documents" should {
+            "display a licences row and a table for the Additional Documents" in {
+              val view = additionalDocumentsSection(Normal, itemWithLicenceReq(answer))(messages)
+              verifyHeader(view)
+              verifyLicenseRow(view, answer)
+              verifyAdditionalDocumentsTable(view)
+            }
           }
         }
-      }
 
-      onJourney(DeclarationType.STANDARD, DeclarationType.SUPPLEMENTARY, DeclarationType.SIMPLIFIED, DeclarationType.OCCASIONAL)(
-        aDeclaration(withItem(item))
-      ) { implicit request =>
-        "no licence required" should {
-          "display section heading" in {
-            val view =
-              additionalDocumentsSection(Mode.Normal, itemWithNoLicenceReq, AdditionalDocuments(Yes, documents), request.declarationType)(messages)
-            val section = view.getElementById("additional-docs-section-item-1")
-
-            section.child(0).text() mustBe messages("declaration.summary.items.item.additionalDocuments")
-
-          }
-
-          "show row with link to `IsLicenceRequiredController`" in {
-
-            val view =
-              additionalDocumentsSection(Mode.Normal, itemWithNoLicenceReq, AdditionalDocuments(Yes, documents), request.declarationType)(messages)
-            val row = view.getElementsByClass("licences-1-row")
-
-            row must haveSummaryKey(messages("declaration.summary.items.item.licences"))
-            row must haveSummaryValue("No")
-
-            row must haveSummaryActionsTexts("site.change", "declaration.summary.items.item.additionalDocuments.changeAll", "1")
-
-            row must haveSummaryActionsHref(routes.IsLicenceRequiredController.displayPage(Mode.Normal, "itemId"))
-
+        onJourney(STANDARD, SUPPLEMENTARY, SIMPLIFIED, OCCASIONAL)(aDeclaration(withItem(itemWithLicenceReq(answer, None)))) { implicit request =>
+          "without Additional Documents" should {
+            "display a licences row" in {
+              val view = additionalDocumentsSection(Normal, itemWithLicenceReq(answer, None))(messages)
+              verifyHeader(view)
+              verifyLicenseRow(view, answer)
+              verifyNoAdditionalDocuments(view, config.getString(is999LKey))
+            }
           }
         }
       }
+    }
 
-      onJourney(DeclarationType.CLEARANCE) { implicit request =>
-        "display section heading" in {
-          val view = additionalDocumentsSection(Mode.Normal, item, AdditionalDocuments(Yes, documents), request.declarationType)(messages)
-          val section = view.getElementById("additional-docs-section-item-1")
+    onJourney(CLEARANCE) { implicit request =>
+      "with actual documents and" when {
 
-          section.child(0).text() mustBe messages("declaration.summary.items.item.clearance.additionalDocuments")
-
-        }
-      }
-
-      "display all additional documents with change buttons" in {
-
-        val view = additionalDocumentsSection(Mode.Normal, item, AdditionalDocuments(Yes, documents), DeclarationType.STANDARD)(messages)
-        val table = view.getElementById("additional-documents-1-table")
-
-        table.getElementsByClass("govuk-table__header").get(0).text() mustBe messages("declaration.summary.items.item.additionalDocuments.code")
-        table.getElementsByClass("govuk-table__header").get(1).text() mustBe messages("declaration.summary.items.item.additionalDocuments.identifier")
-        table.getElementsByClass("govuk-table__header").get(2).text() mustBe messages("site.change.header")
-
-        val row1 = table.getElementsByClass("govuk-table__body").first().getElementsByClass("govuk-table__row").get(0)
-        row1.getElementsByClass("govuk-table__cell").get(0).text() mustBe "typ1"
-        row1.getElementsByClass("govuk-table__cell").get(1).text() mustBe "identifier1"
-        val row1ChangeLink = row1.getElementsByClass("govuk-table__cell").get(2).getElementsByTag("a").first()
-        row1ChangeLink must haveHref(routes.AdditionalDocumentsController.displayPage(Mode.Normal, "itemId"))
-        row1ChangeLink must containMessage("site.change")
-        row1ChangeLink must containMessage("declaration.summary.items.item.additionalDocuments.change", "typ1", "identifier1", 1)
-
-        val row2 = table.getElementsByClass("govuk-table__body").first().getElementsByClass("govuk-table__row").get(1)
-        row2.getElementsByClass("govuk-table__cell").get(0).text() mustBe "typ2"
-        row2.getElementsByClass("govuk-table__cell").get(1).text() mustBe "identifier2"
-        val row2ChangeLink = row2.getElementsByClass("govuk-table__cell").get(2).getElementsByTag("a").first()
-        row2ChangeLink must haveHref(routes.AdditionalDocumentsController.displayPage(Mode.Normal, "itemId"))
-        row2ChangeLink must containMessage("site.change")
-        row2ChangeLink must containMessage("declaration.summary.items.item.additionalDocuments.change", "typ2", "identifier2", 1)
-      }
-
-      "display all additional documents without change buttons" when {
-
-        "actionsEnabled is false" in {
-
-          val view =
-            additionalDocumentsSection(Mode.Normal, item, AdditionalDocuments(Yes, documents), DeclarationType.STANDARD, actionsEnabled = false)(
-              messages
-            )
-          val table = view.getElementById("additional-documents-1-table")
-
-          table.getElementsByClass("govuk-table__header").get(0).text() mustBe messages("declaration.summary.items.item.additionalDocuments.code")
-          table.getElementsByClass("govuk-table__header").get(1).text() mustBe messages(
-            "declaration.summary.items.item.additionalDocuments.identifier"
-          )
-
-          val row1 = table.getElementsByClass("govuk-table__body").first().getElementsByClass("govuk-table__row").get(0)
-          row1.getElementsByClass("govuk-table__cell").get(0).text() mustBe "typ1"
-          row1.getElementsByClass("govuk-table__cell").get(1).text() mustBe "identifier1"
-          val row1ChangeLink = row1.getElementsByClass("govuk-table__cell").get(2)
-          row1ChangeLink.attr("href") mustBe empty
-          row1ChangeLink.text() mustBe empty
-
-          val row2 = table.getElementsByClass("govuk-table__body").first().getElementsByClass("govuk-table__row").get(1)
-          row2.getElementsByClass("govuk-table__cell").get(0).text() mustBe "typ2"
-          row2.getElementsByClass("govuk-table__cell").get(1).text() mustBe "identifier2"
-          val row2ChangeLink = row2.getElementsByClass("govuk-table__cell").get(2)
-          row2ChangeLink.attr("href") mustBe empty
-          row2ChangeLink.text() mustBe empty
+        "actions are enabled" should {
+          "not have a 'License' section and have 'Change' buttons" in {
+            val view = additionalDocumentsSection(Normal, itemWithNoLicenceReq())(messages)
+            verifyHeader(view)
+            view.getElementsByClass("licences-1-row") mustBe empty
+            verifyAdditionalDocumentsTable(view)
+          }
         }
 
+        "actions are disabled" should {
+          "not have a 'License' section and not have 'Change' buttons" in {
+            val view = additionalDocumentsSection(Normal, itemWithNoLicenceReq(), actionsEnabled = false)(messages)
+            verifyHeader(view)
+            verifyAdditionalDocumentsTable(view, false)
+          }
+        }
       }
-
     }
   }
 
-  "Supporting documents view with 999l disabled" when {
+  "AdditionalDocuments view" when {
+    "with additionalDocuments defined but without actual documents or defined licence answer" should {
+      "only display title and 'Other Documents' as header in the 'No Additional Documents' section" when {
+        "the '999l' flag is disabled" in {
+          val is999LKey = "microservice.services.features.waiver999L"
+          val config = ConfigFactory.parseString(s"$is999LKey=disabled")
+          val injector = new Injector {
+            override val configuration: Configuration = Configuration(config)
+          }
+          val additionalDocumentsSection = injector.instanceOf[additional_documents]
+          val view = additionalDocumentsSection(Normal, itemWithNoLicenceReq(Some(AdditionalDocuments(None, Seq.empty))))(messages)
+          verifyHeader(view)
 
-    val injector = new Injector {
-      override val configuration: Configuration = Configuration(ConfigFactory.parseString("microservice.services.features.waiver999L=disabled"))
-    }
-    val additionalDocumentsSection = injector.instanceOf[additional_documents]
+          view.getElementsByClass("licences-1-row") mustBe empty
+          view.getElementsByTag("table") mustBe empty
 
-    "with documents" should {
-
-      "display section heading" in {
-
-        val view = additionalDocumentsSection(Mode.Normal, item, AdditionalDocuments(Yes, documents), DeclarationType.STANDARD)(messages)
-        val section = view.getElementById("additional-docs-section-item-1")
-
-        section.child(0).firstElementSibling().text() mustBe messages("declaration.summary.items.item.clearance.additionalDocuments")
+          verifyNoAdditionalDocuments(view, config.getString(is999LKey))
+        }
       }
     }
-
   }
 
+  private def verifyHeader(view: Appendable): Assertion = {
+    val section = view.getElementById("additional-docs-section-item-1")
+    section.child(0).tagName mustBe "h3"
+    section.child(0).text mustBe messages("declaration.summary.items.item.additionalDocuments")
+  }
+
+  private def verifyLicenseRow(view: Appendable, expectedValue: String): Assertion = {
+    val licencesRow = view.getElementsByClass("licences-1-row")
+    licencesRow must haveSummaryKey(messages("declaration.summary.items.item.licences"))
+    licencesRow must haveSummaryValue(expectedValue)
+    licencesRow must haveSummaryActionsTexts("site.change", "declaration.summary.items.item.additionalDocuments.changeAll", "1")
+    licencesRow must haveSummaryActionsHref(routes.IsLicenceRequiredController.displayPage(Normal, "itemId"))
+  }
+
+  private def verifyNoAdditionalDocuments(view: Appendable, flag999L: String): Assertion = {
+    val is999L = flag999L == "enabled"
+    val row = view.getElementsByClass("additional-documents-1-row")
+    row must haveSummaryKey(messages(s"declaration.summary.items.item.additionalDocuments${if (is999L) "" else ".otherDocuments"}"))
+    row must haveSummaryValue("No")
+    row must haveSummaryActionsTexts("site.change", "declaration.summary.items.item.additionalDocuments.changeAll", "1")
+    row must haveSummaryActionsHref(routes.AdditionalDocumentsController.displayPage(Normal, "itemId"))
+  }
+
+  private def verifyAdditionalDocumentsTable(view: Appendable, actionsEnabled: Boolean = true): Assertion = {
+    val table = view.getElementById("additional-documents-1-table")
+
+    val tableHeader = table.getElementsByClass("govuk-table__header")
+    tableHeader.get(0).text mustBe messages("declaration.summary.items.item.additionalDocuments.code")
+    tableHeader.get(1).text mustBe messages("declaration.summary.items.item.additionalDocuments.identifier")
+    tableHeader.get(2).text mustBe messages("site.change.header")
+
+    val rows = table.getElementsByClass("govuk-table__body").first.getElementsByClass("govuk-table__row")
+    verifyAdditionalDocumentsRow(rows, 1, actionsEnabled)
+    verifyAdditionalDocumentsRow(rows, 2, actionsEnabled)
+  }
+
+  private def verifyAdditionalDocumentsRow(rows: Elements, id: Int, actionsEnabled: Boolean): Assertion = {
+    val row = rows.get(id - 1)
+    row.getElementsByClass("govuk-table__cell").get(0).text mustBe s"typ$id"
+    row.getElementsByClass("govuk-table__cell").get(1).text mustBe s"identifier$id"
+    val rowChange = row.getElementsByClass("govuk-table__cell").get(2)
+    if (actionsEnabled) {
+      val rowChangeLink = rowChange.getElementsByTag("a").first
+      rowChangeLink must haveHref(routes.AdditionalDocumentsController.displayPage(Normal, "itemId"))
+      rowChangeLink must containMessage("site.change")
+      rowChangeLink must containMessage("declaration.summary.items.item.additionalDocuments.change", s"typ$id", s"identifier$id", 1)
+    } else {
+      rowChange.attr("href") mustBe empty
+      rowChange.text mustBe empty
+    }
+  }
 }
