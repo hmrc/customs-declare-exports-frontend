ea78256fe36ab289cdb8378f06dea95e2e030df7
darrylmurphy <darrylmurphy@users.noreply.github.com>
2022-11-16 09:44:54 +0000

    CEDS-3489: remove playfop dependency

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -12,7 +12,6 @@ object AppDependencies {
     "ai.x" %% "play-json-extensions" % "0.42.0",
     "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.13.3",
     "com.github.tototoshi" %% "scala-csv" % "1.3.10",
-    "com.dmanchester" %% "playfop" % "1.0",
     "net.sf.barcode4j" % "barcode4j" % "2.1",
     "org.webjars.npm" % "accessible-autocomplete" % "2.0.4"
   ).map(_.withSources)
