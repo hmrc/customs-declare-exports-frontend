7bb66f21a35874343ac5ffb737e863052c2b4f79
Tom Robinson <25112739+magicaljellybeans@users.noreply.github.com>
2022-12-05 17:09:39 +0000

    CEDS-4262 Amend DUCR constructor so it always passes EORI digits only

--- app/controllers/declaration/TraderReferenceController.scala
+++ app/controllers/declaration/TraderReferenceController.scala
@@ -66,7 +66,7 @@ class TraderReferenceController @Inject() (
     val lastDigitOfYear = request.cacheModel.createdDateTime.atZone(ZoneId.of("Europe/London")).getYear.toString.last
     val eori = request.eori.toUpperCase
 
-    Ducr(lastDigitOfYear + "GB" + eori.takeRight(12) + "-" + traderReference.value)
+    Ducr(lastDigitOfYear + "GB" + eori.dropWhile(_.isLetter) + "-" + traderReference.value)
   }
 
   private def updateCache(generatedDucr: Ducr)(implicit request: JourneyRequest[_]): Future[ExportsDeclaration] = {
