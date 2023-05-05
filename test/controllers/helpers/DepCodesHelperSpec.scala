4ee27eb0afbc8ca7f1f1a07d69eebf6a71c62f89
Tim <glidester@gmail.com>
2023-05-05 09:41:18 +0100

    Test fix

--- test/controllers/helpers/DepCodesHelperSpec.scala
+++ test/controllers/helpers/DepCodesHelperSpec.scala
@@ -20,23 +20,41 @@ import base.{MockExportCacheService, RealConfig, UnitWithMocksSpec}
 import config.AppConfig
 import connectors.{FileBasedCodeListConnector, GoodsLocationCodesConnector}
 import forms.declaration.LocationOfGoods
+import org.mockito.Mockito.{reset, when}
 import play.api.{Environment, Mode}
 import utils.JsonFile
 
 class DepCodesHelperSpec extends UnitWithMocksSpec with MockExportCacheService with RealConfig {
 
-  private val appConfig: AppConfig = appConfig(configuration, Environment.simple(mode = Mode.Test))
+  private val appConfig = mock[AppConfig]
   private lazy val jsonFile = new JsonFile(Environment.simple(mode = Mode.Test))
   private lazy val glcConnector = new GoodsLocationCodesConnector(appConfig, jsonFile)
   private lazy val codeListConnector = new FileBasedCodeListConnector(appConfig, glcConnector, jsonFile)
   private lazy val depCodesHelper = new DepCodesHelper(codeListConnector)
+
+  override def beforeEach(): Unit = {
+    super.beforeEach()
+
+    reset(appConfig)
+    when(appConfig.glcDep16k).thenReturn("/code-lists/manyCodes.json")
+    when(appConfig.holderOfAuthorisationCodeFile).thenReturn("/code-lists/manyCodes.json")
+    when(appConfig.procedureCodesListFile).thenReturn("/code-lists/manyCodes.json")
+    when(appConfig.procedureCodesForC21ListFile).thenReturn("/code-lists/manyCodes.json")
+    when(appConfig.additionalProcedureCodes).thenReturn("/code-lists/manyCodes.json")
+    when(appConfig.additionalProcedureCodesForC21).thenReturn("/code-lists/manyCodes.json")
+    when(appConfig.dmsErrorCodes).thenReturn("/code-lists/manyCodes.json")
+    when(appConfig.countryCodes).thenReturn("/code-lists/manyCodes.json")
+    when(appConfig.packageTypeCodeFile).thenReturn("/code-lists/manyCodes.json")
+    when(appConfig.officeOfExitsCodeFile).thenReturn("/code-lists/manyCodes.json")
+    when(appConfig.customsOfficesCodeFile).thenReturn("/code-lists/manyCodes.json")
+  }
 
   "DepCodesHelper on isDesignatedExportPlaceCode" should {
 
     "return true" when {
 
       "Goods location code on declaration is a valid DEP code" in {
-        val declaration = aDeclaration(withGoodsLocation(LocationOfGoods("GBAUBA5ABDCGD")))
+        val declaration = aDeclaration(withGoodsLocation(LocationOfGoods("001")))
 
         depCodesHelper.isDesignatedExportPlaceCode(declaration) must be(true)
       }
