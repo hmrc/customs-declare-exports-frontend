86cb7d831424ecf8ebda0af322740de4c3a80694
Mohammad Dweik <89045034+Dweik97@users.noreply.github.com>
2023-07-25 11:42:44 +0100

    CEDS-4739: update the play-frontend-hmrc to version 7.4.0-play-28

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,7 +9,7 @@ object AppDependencies {
     "uk.gov.hmrc"                  %% "bootstrap-frontend-play-28"    % bootstrapPlayVersion,
     "uk.gov.hmrc"                  %% "play-allowlist-filter"         % "1.1.0",
     "uk.gov.hmrc"                  %% "play-conditional-form-mapping" % "1.12.0-play-28",
-    "uk.gov.hmrc"                  %% "play-frontend-hmrc"            % "6.4.0-play-28",
+    "uk.gov.hmrc"                  %% "play-frontend-hmrc"            % "7.4.0-play-28",
     "uk.gov.hmrc"                  %% "play-partials"                 % "8.3.0-play-28",
     "ai.x"                         %% "play-json-extensions"          % "0.42.0",
     "com.fasterxml.jackson.module" %% "jackson-module-scala"          % "2.14.2",
