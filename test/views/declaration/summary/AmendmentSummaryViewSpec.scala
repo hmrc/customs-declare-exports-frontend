d9453bef77ecd18f30302ca66df95ee95a69500e
Mohammad Dweik <89045034+Dweik97@users.noreply.github.com>
2023-12-14 10:48:05 +0000

    unit test

--- test/views/declaration/summary/AmendmentSummaryViewSpec.scala
+++ test/views/declaration/summary/AmendmentSummaryViewSpec.scala
@@ -16,11 +16,15 @@
 
 package views.declaration.summary
 
-import controllers.routes.DeclarationDetailsController
+import controllers.routes.{DeclarationDetailsController, SavedDeclarationsController}
 import models.ExportsDeclaration
 import models.declaration.DeclarationStatus.AMENDMENT_DRAFT
 import play.twirl.api.HtmlFormat.Appendable
 import views.html.declaration.amendments.amendment_summary
+import base.ExportsTestData.newUser
+import models.requests.JourneyRequest
+import play.api.test.FakeRequest
+import utils.FakeRequestCSRFSupport.CSRFFakeRequest
 
 class AmendmentSummaryViewSpec extends SummaryViewSpec {
 
@@ -43,11 +47,26 @@ class AmendmentSummaryViewSpec extends SummaryViewSpec {
       view.getElementById("title").text() mustBe messages("declaration.summary.amendment-draft-header")
     }
 
-    "should display correct back link" in {
-      val backButton = view.getElementById("back-link")
+    "should display correct back link" when {
+      "referer is the /saved-declarations page" in {
+        val fakeReferer = Map("referer" -> ".../saved-declarations")
+        val journeyWithHeader = new JourneyRequest(
+          buildVerifiedEmailRequest(FakeRequest("", "").withHeaders(fakeReferer.toSeq: _*).withCSRFToken, newUser("12345", "12345")),
+          declaration
+        )
+        val viewWithHeaders = amendmentSummaryPage(submissionId)(journeyWithHeader, messages, minimalAppConfig)
+        val backButton = viewWithHeaders.getElementById("back-link")
 
-      backButton.text() mustBe messages("site.backToDeclarations")
-      backButton must haveHref(DeclarationDetailsController.displayPage(submissionId).url)
+        backButton.text() mustBe messages("site.back")
+        backButton must haveHref(SavedDeclarationsController.displayDeclarations().url)
+      }
+
+      "referer is otherwise i.e. /information page or does not exist" in {
+        val backButton = view.getElementById("back-link")
+
+        backButton.text() mustBe messages("site.back")
+        backButton must haveHref(DeclarationDetailsController.displayPage(submissionId).url)
+      }
     }
 
     "warning text should be displayed" in {
