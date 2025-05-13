32ed4031017cfbc8d35c7e59f6b0aae5d27c5047
andrew-bintcliffe <186038219+andrew-bintcliffe@users.noreply.github.com>
2025-05-13 14:52:11 +0100

    DCWL-3212 | Upgrade Dependency play-conditional-form-mapping-play-30

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     "uk.gov.hmrc"                    %% "bootstrap-frontend-play-30"             % bootstrapPlayVersion,
     "uk.gov.hmrc"                    %% "play-frontend-hmrc-play-30"             % frontendPlayVersion,
     "uk.gov.hmrc"                    %% "play-allowlist-filter"                  % "1.3.0",
-    "uk.gov.hmrc"                    %% "play-conditional-form-mapping-play-30"  % "3.2.0",
+    "uk.gov.hmrc"                    %% "play-conditional-form-mapping-play-30"  % "3.3.0",
     "com.fasterxml.jackson.module"   %% "jackson-module-scala"                   % "2.18.1",
     "net.sf.barcode4j"               %  "barcode4j"                              % "2.1",
     "org.webjars.npm"                %  "accessible-autocomplete"                % "3.0.0",
