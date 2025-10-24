bb6a504aaca22130e5f4bbbd4eda0935bbfc917b
jack-brannon <166420733+Jack-Brannon@users.noreply.github.com>
2025-10-24 18:12:07 +0100

    DCWL-4048 | Making statistical value field return None where left blank by user

--- app/controllers/section5/StatisticalValueController.scala
+++ app/controllers/section5/StatisticalValueController.scala
@@ -66,15 +66,18 @@ class StatisticalValueController @Inject() (
         .bindFromRequest()
         .fold(
           (formWithErrors: Form[StatisticalValue]) => Future.successful(BadRequest(itemTypePage(itemId, formWithErrors))),
-          updateExportsCache(itemId, _).map(_ => navigator.continueTo(PackageInformationSummaryController.displayPage(itemId)))
+          statisticalValue => {
+            val statisticalValueOption = if (statisticalValue.statisticalValue.trim.isEmpty) None else Some(statisticalValue)
+            updateExportsCache(itemId, statisticalValueOption).map(_ => navigator.continueTo(PackageInformationSummaryController.displayPage(itemId)))
+          }
         )
   }
 
   private def redirectToRoot(itemId: String)(implicit request: JourneyRequest[_]): Boolean =
     occasionalAndSimplified.contains(request.declarationType) && !request.cacheModel.isLowValueDeclaration(itemId)
 
-  private def updateExportsCache(itemId: String, updatedItem: StatisticalValue)(
+  private def updateExportsCache(itemId: String, updatedItem: Option[StatisticalValue])(
     implicit request: JourneyRequest[AnyContent]
   ): Future[ExportsDeclaration] =
-    updateDeclarationFromRequest(_.updatedItem(itemId, item => item.copy(statisticalValue = Some(updatedItem))))
+    updateDeclarationFromRequest(_.updatedItem(itemId, item => item.copy(statisticalValue = updatedItem)))
 }
