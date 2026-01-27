6d0f27b46cbfc5d449f48dab9abf98eda66cdafd
TJSarno <thomassarno@hotmail.com>
2026-01-27 13:28:10 +0000

    DCWL-3998, added handling of nones in summary page

--- app/views/helpers/summary/PackageInformationHelper.scala
+++ app/views/helpers/summary/PackageInformationHelper.scala
@@ -16,6 +16,7 @@
 
 package views.helpers.summary
 
+import config.AppConfig
 import controllers.section5.routes.PackageInformationSummaryController
 import forms.section5.PackageInformation
 import models.declaration.ExportItem
@@ -26,12 +27,18 @@ import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist._
 import javax.inject.{Inject, Singleton}
 
 @Singleton
-class PackageInformationHelper @Inject() (packageTypesService: PackageTypesService) extends SummaryHelper {
+class PackageInformationHelper @Inject() (packageTypesService: PackageTypesService, appConfig: AppConfig) extends SummaryHelper {
 
-  def maybeSummarySection(item: ExportItem, actionsEnabled: Boolean, itemIndex: Int)(implicit messages: Messages): Option[SummarySection] =
+  def maybeSummarySection(item: ExportItem, actionsEnabled: Boolean, itemIndex: Int)(implicit messages: Messages):
+  Option[SummarySection] =
     item.packageInformation.map { listOfPackageInformation =>
       val summaryListRows = listOfPackageInformation.zipWithIndex.flatMap { case (packageInfo, index) =>
+        if(appConfig.isOptionalFieldsEnabled){
         packageInfoRowsOpt(item, itemIndex, packageInfo, index + 1, actionsEnabled)
+        }
+        else {
+          packageInfoRows(item, itemIndex, packageInfo, index + 1, actionsEnabled)
+        }
       }.flatten
 
       if (summaryListRows.isEmpty) headingOnNoPackageInfo(item, actionsEnabled, itemIndex)
@@ -50,39 +57,39 @@ class PackageInformationHelper @Inject() (packageTypesService: PackageTypesServi
       )
     )
 
-//  private def packageInfoRows(item: ExportItem, itemIndex: Int, packageInfo: PackageInformation, index: Int, actionsEnabled: Boolean)(
-//    implicit messages: Messages
-//  ): Seq[Option[SummaryListRow]] =
-//    List(
-//      packageInfo.typesOfPackages.map { _ =>
-//        SummaryListRow(
-//          key("item.packageInformation.type"),
-//          value(packageTypesService.typesOfPackagesText(packageInfo.typesOfPackages).getOrElse("")),
-//          classes = s"${noBorder(false, packageInfo)}item-$itemIndex-package-information-$index-type",
-//          changePackageInformation(item, actionsEnabled, itemIndex)
-//        )
-//      },
-//      packageInfo.numberOfPackages.map { numberOfPackages =>
-//        SummaryListRow(
-//          key("item.packageInformation.number"),
-//          value(numberOfPackages.toString),
-//          classes = s"${noBorder(true, packageInfo)}item-$itemIndex-package-information-$index-number",
-//          packageInfo.typesOfPackages.fold(changePackageInformation(item, actionsEnabled, itemIndex))(_ => None)
-//        )
-//      },
-//      packageInfo.shippingMarks.map { shippingMarks =>
-//        SummaryListRow(
-//          key("item.packageInformation.markings"),
-//          value(shippingMarks),
-//          classes = s"package-info item-$itemIndex-package-information-$index-markings", {
-//            val changeLinkOnShippingMarks = packageInfo.typesOfPackages.isEmpty && packageInfo.numberOfPackages.isEmpty
-//            if (changeLinkOnShippingMarks) changePackageInformation(item, actionsEnabled, itemIndex) else None
-//          }
-//        )
-//      }
-//    )
+  private def packageInfoRows(item: ExportItem, itemIndex: Int, packageInfo: PackageInformation, index: Int, actionsEnabled: Boolean)(
+    implicit messages: Messages
+  ): Seq[Option[SummaryListRow]] =
+    List(
+      packageInfo.typesOfPackages.map { _ =>
+        SummaryListRow(
+          key("item.packageInformation.type"),
+          value(packageTypesService.typesOfPackagesText(packageInfo.typesOfPackages).getOrElse("")),
+          classes = s"${noBorder(false, packageInfo)}item-$itemIndex-package-information-$index-type",
+          changePackageInformation(item, actionsEnabled, itemIndex)
+        )
+      },
+      packageInfo.numberOfPackages.map { numberOfPackages =>
+        SummaryListRow(
+          key("item.packageInformation.number"),
+          value(numberOfPackages.toString),
+          classes = s"${noBorder(true, packageInfo)}item-$itemIndex-package-information-$index-number",
+          packageInfo.typesOfPackages.fold(changePackageInformation(item, actionsEnabled, itemIndex))(_ => None)
+        )
+      },
+      packageInfo.shippingMarks.map { shippingMarks =>
+        SummaryListRow(
+          key("item.packageInformation.markings"),
+          value(shippingMarks),
+          classes = s"package-info item-$itemIndex-package-information-$index-markings", {
+            val changeLinkOnShippingMarks = packageInfo.typesOfPackages.isEmpty && packageInfo.numberOfPackages.isEmpty
+            if (changeLinkOnShippingMarks) changePackageInformation(item, actionsEnabled, itemIndex) else None
+          }
+        )
+      }
+    )
 
-
+// Remove above, implement below when removing flag
   private def packageInfoRowsOpt(item: ExportItem, itemIndex: Int, packageInfo: PackageInformation, index: Int, actionsEnabled: Boolean)(
     implicit messages: Messages
   ): Seq[Option[SummaryListRow]] =
@@ -92,21 +99,19 @@ class PackageInformationHelper @Inject() (packageTypesService: PackageTypesServi
           key("item.packageInformation.number"),
           value(numberOfPackages.toString),
           classes = s"${noBorder(false, packageInfo)}item-$itemIndex-package-information-$index-number",
-          packageInfo.typesOfPackages.fold(changePackageInformation(item, actionsEnabled, itemIndex))(_ => None)
+          changePackageInformation(item, actionsEnabled, itemIndex)
         )
       },
       packageInfo.typesOfPackages.map { _ =>
         SummaryListRow(
           key("item.packageInformation.type"),
           value(packageTypesService.typesOfPackagesText(packageInfo.typesOfPackages).getOrElse("")),
-          classes = s"${noBorder(false, packageInfo)}item-$itemIndex-package-information-$index-type",
-          changePackageInformation(item, actionsEnabled, itemIndex)
+          classes = s"${noBorder(false, packageInfo)}item-$itemIndex-package-information-$index-type"
         )
       }.orElse(Some(SummaryListRow(
         key("item.packageInformation.type"),
-        value("TEST"),
-        classes = s"${noBorder(false, packageInfo)}item-$itemIndex-package-information-$index-type",
-        changePackageInformation(item, actionsEnabled, itemIndex)
+        value(messages("declaration.summary.not.provided")),
+        classes = s"${noBorder(false, packageInfo)}item-$itemIndex-package-information-$index-type"
       ))),
       packageInfo.shippingMarks.map { shippingMarks =>
         SummaryListRow(
@@ -119,7 +124,7 @@ class PackageInformationHelper @Inject() (packageTypesService: PackageTypesServi
         )
       }.orElse(Some(SummaryListRow(
         key("item.packageInformation.markings"),
-        value("TEST"),
+        value(messages("declaration.summary.not.provided")),
         classes = s"package-info item-$itemIndex-package-information-$index-markings", {
           val changeLinkOnShippingMarks = packageInfo.typesOfPackages.isEmpty && packageInfo.numberOfPackages.isEmpty
           if (changeLinkOnShippingMarks) changePackageInformation(item, actionsEnabled, itemIndex) else None
@@ -127,16 +132,11 @@ class PackageInformationHelper @Inject() (packageTypesService: PackageTypesServi
       )))
     )
 
-//  private def noBorder(isNumberOfPackages: Boolean, packageInfo: PackageInformation): String =
-//    if (isNumberOfPackages) packageInfo.shippingMarks.fold("")(_ => "govuk-summary-list__row--no-border ")
-//    else if (packageInfo.numberOfPackages.isEmpty && packageInfo.shippingMarks.isEmpty) ""
-//    else "govuk-summary-list__row--no-border "
-
   private def noBorder(isNumberOfPackages: Boolean, packageInfo: PackageInformation): String =
     if (isNumberOfPackages) packageInfo.shippingMarks.fold("")(_ => "govuk-summary-list__row--no-border ")
     else if (packageInfo.numberOfPackages.isEmpty && packageInfo.shippingMarks.isEmpty) ""
     else "govuk-summary-list__row--no-border "
 
   private def changePackageInformation(item: ExportItem, actionsEnabled: Boolean, itemIndex: Int)(implicit messages: Messages): Option[Actions] =
-    changeLink(PackageInformationSummaryController.displayPage(item.id), "item.packageInformation", actionsEnabled, Some(itemIndex))
+    changeLink(PackageInformationSummaryController.displayPage(item.id), "item.packageInformation.number", actionsEnabled, Some(itemIndex))
 }
